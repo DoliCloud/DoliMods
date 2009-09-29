a30945d8b39c971d4d72e5c63fab29d0d90fa363
Laurent Destailleur <eldy@users.sourceforge.net>
2009-09-29 17:45:27 +0000

    Qual: Remove deprecated files and code from main repository

---
+++ htdocs/voyage/voyage.php
@@ -0,0 +1,28 @@
+<?php
+/* Copyright (C) 2001-2002 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id: voyage.php,v 1.1 2009/09/29 17:45:27 eldy Exp $
+ * $Source: /cvsroot/dolibarr/dolibarrmod/htdocs/voyage/voyage.php,v $
+ *
+ */
+
+require("./pre.inc.php");
+
+llxHeader();
+
+llxFooter("<em>Derni&egrave;re modification $Date: 2009/09/29 17:45:27 $ r&eacute;vision $Revision: 1.1 $</em>");
+?>
