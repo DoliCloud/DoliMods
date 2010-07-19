1fec4a5bc7fef1f09559255d491f80c7d5d93eee
Laurent Destailleur <eldy@users.sourceforge.net>
2010-07-19 18:45:43 +0000

    New: Add PHPSysinfo plugin

---
+++ htdocs/phpsysinfo/inc/phpsysinfo/plugins/PS/PS.config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * PS Plugin Config File
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PSI_Plugin_PS
+ * @author    Michael Cramer <BigMichi1@users.sourceforge.net>
+ * @copyright 2009 phpSysInfo
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @version   SVN: $Id: PS.config.php,v 1.1 2010/07/19 18:45:48 eldy Exp $
+ * @link      http://phpsysinfo.sourceforge.net
+ */
+ /**
+ * define how to access the ps statistic data
+ * - 'command' ps command is run everytime the block gets refreshed or build / on WINNT information is retrieved everytime through WMI
+ * - 'data' (a file must be available in the data directory of the phpsysinfo installation with the filename "ps.txt"; content is the output from "ps -axo pid,ppid,pmem,args")
+ */
+define('PSI_PLUGIN_PS_ACCESS', 'command');
+?>
---
+++ htdocs/phpsysinfo/index.php
@@ -0,0 +1,70 @@
+<?php
+/* Copyright (C) 2004-2010 Laurent Destailleur <eldy@users.sourceforge.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id: index.php,v 1.1 2010/07/19 18:45:45 eldy Exp $
+ */
+
+/**	    \file       htdocs/phpsysinfo/frames.php
+        \ingroup    phpsysinfo
+		\brief      Page that build two frames: One for menu, the other for the target page to show
+		\author	    Laurent Destailleur
+		\version    $Revision: 1.1 $
+*/
+
+$res=@include("../main.inc.php");
+if (! $res) $res=@include("../../main.inc.php");	// If pre.inc.php is called by jawstats
+if (! $res) $res=@include("../../../dolibarr/htdocs/main.inc.php");		// Used on dev env only
+if (! $res) $res=@include("../../../../dolibarr/htdocs/main.inc.php");	// Used on dev env only
+
+$langs->load("phpsysinfo@phpsysinfo");
+
+$mainmenu=isset($_GET["mainmenu"])?$_GET["mainmenu"]:"";
+$leftmenu=isset($_GET["leftmenu"])?$_GET["leftmenu"]:"";
+$idmenu=isset($_GET["idmenu"])?$_GET["idmenu"]:"";
+$theme=isset($_GET["theme"])?$_GET["theme"]:"";
+$codelang=isset($_GET["lang"])?$_GET["lang"]:"";
+
+print "
+<html>
+<head>
+<title>Dolibarr frame for external web site</title>
+</head>
+
+<frameset rows=\"".$heightforframes.",*\" border=0 framespacing=0 frameborder=0>
+    <frame name=\"barre\" src=\"frametop.php?mainmenu=".$mainmenu."&leftmenu=".$leftmenu."&idmenu=".$idmenu.($theme?'&theme='.$theme:'').($codelang?'&lang='.$codelang:'')."&nobackground=1\" noresize scrolling=\"NO\" noborder>
+    <frame name=\"main\" src=\"./inc/phpsysinfo/index.php\">
+    <noframes>
+    <body>
+
+    </body>
+    </noframes>
+</frameset>
+
+<noframes>
+<body>
+	<br><center>
+	Sorry, your browser is too old or not correctly configured to view this area.<br>
+	Your browser must support frames.<br>
+	</center>
+</body>
+</noframes>
+
+</html>
+";
+
+
+?>
