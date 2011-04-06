c67440efeb401c4a78f8ad5c9c283fd55b3c9c66
Laurent Destailleur <eldy@users.sourceforge.net>
2011-04-06 11:19:16 +0000

    Qual: Make module system more simple. Change to prepare adding left menu entries by modules

--- htdocs/includes/menus/cabinetmed.lib.php
+++ htdocs/includes/menus/cabinetmed.lib.php
@@ -21,7 +21,7 @@
 /**
  *  \file		htdocs/includes/menus/cabinetmed.lib.php
  *  \brief		Library for file cabinetmed menus
- *  \version	$Id: cabinetmed.lib.php,v 1.12 2011/04/06 11:12:33 eldy Exp $
+ *  \version	$Id: cabinetmed.lib.php,v 1.13 2011/04/06 11:19:16 eldy Exp $
  */
 
 
@@ -1307,17 +1307,7 @@ function print_left_cabinetmed_menu($db,$menu_array_before,$menu_array_after)
         require_once(DOL_DOCUMENT_ROOT."/core/class/menubase.class.php");
 
         $menuArbo = new Menubase($db,'cabinetmed','left');
-        //$overwritemenufor = $menuArbo->listeMainmenu();
-        // Add other mainmenu to the list of menu to overwrite pre.inc.php
-        //$overwritemenumore=array('home','companies','members','products','suppliers','commercial','accountancy','agenda','project','tools','ecm');
-        //$overwritemenufor=array_merge($overwritemenumore, $overwritemenufor);
         $newmenu = $menuArbo->menuLeftCharger($newmenu,$mainmenu,$leftmenu,($user->societe_id?1:0),'cabinetmed');
-
-        /*
-         * Menu AUTRES (Pour les menus du haut qui ne serait pas geres)
-         */
-        //if ($mainmenu && ! in_array($mainmenu,$overwritemenufor)) { $mainmenu=""; }
-
     }
 
 
@@ -1326,13 +1316,11 @@ function print_left_cabinetmed_menu($db,$menu_array_before,$menu_array_after)
      */
     //var_dump($menu_array_before);exit;
     //var_dump($menu_array_after);exit;
-    //if ($mainmenu) {
     $menu_array=$newmenu->liste;
     if (is_array($menu_array_before)) $menu_array=array_merge($menu_array_before, $menu_array);
     if (is_array($menu_array_after))  $menu_array=array_merge($menu_array, $menu_array_after);
-    //}
 
-    // Affichage du menu
+    // Show menu
     $alt=0;
     if (is_array($menu_array))
     {
