26cd4c6e4a0f921a86ab69cf558d3781cbc6ada4
Laurent Destailleur <eldy@users.sourceforge.net>
2011-08-10 10:21:23 +0000

    Prepare release of concatpdf module

--- htdocs/concatpdf/admin/concatpdf.php
+++ htdocs/concatpdf/admin/concatpdf.php
@@ -1,5 +1,5 @@
 <?php
-/* Copyright (C) 2008-2009 Laurent Destailleur  <eldy@users.sourceforge.net>
+/* Copyright (C) 2008-2011 Laurent Destailleur  <eldy@users.sourceforge.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,9 +18,9 @@
 
 /**
  *	    \file       htdocs/concatpdf/admin/concatpdf.php
- *      \ingroup    cabinetmed
+ *      \ingroup    concatpdf
  *      \brief      Page to setup module ConcatPdf
- *		\version    $Id: concatpdf.php,v 1.2 2011/06/15 17:30:32 eldy Exp $
+ *		\version    $Id: concatpdf.php,v 1.3 2011/08/10 10:21:23 eldy Exp $
  */
 
 define('NOCSRFCHECK',1);
@@ -56,38 +56,7 @@ $actionsave=GETPOST("save");
  * Actions
  */
 
-// Save parameters
-if ($actionsave)
-{
-	$error=0;
-	$i=0;
-
-	$db->begin();
-
-	/*    if (! preg_match('|[\\\/]$|',$_POST["RRD_COMMANDLINE_TOOL"]))
-	 {
-	 $mesg="<div class=\"error\">".$langs->trans("ErrorRrdDataDirMustEndWithASlash")."</div>";
-	 $error++;
-	 }
-	 */
-	if (! $error)
-	{
-		if ($i >= 0) $i+=dolibarr_set_const($db,'MONITORING_COMMANDLINE_TOOL',trim($_POST["MONITORING_COMMANDLINE_TOOL"]),'chaine',0);
-
-		if ($i >= 1)
-		{
-			$db->commit();
-			$mesg = "<div class=\"ok\">".$langs->trans("SetupSaved")."</div>";
-		}
-		else
-		{
-			$db->rollback();
-			$mesg=$db->lasterror();
-			//header("Location: ".$_SERVER["PHP_SELF"]);
-			//exit;
-		}
-	}
-}
+// None
 
 
 /**
@@ -100,38 +69,7 @@ $linkback='<a href="'.DOL_URL_ROOT.'/admin/modules.php">'.$langs->trans("BackToM
 print_fiche_titre($langs->trans("ConcatPdfSetup"),$linkback,'setup');
 print '<br>';
 
-/*
-print '<form name="rrdform" action="'.$_SERVER["PHP_SELF"].'" method="post">';
-print "<table class=\"noborder\" width=\"100%\">";
-$var=true;
-
-print "<tr class=\"liste_titre\">";
-print "<td>".$langs->trans("Parameter")."</td>";
-print "<td>".$langs->trans("Value")."</td>";
-print "<td>".$langs->trans("Examples")."</td>";
-print "</tr>";
-
-$var=!$var;
-print "<tr ".$bc[$var].">";
-print "<td>".$langs->trans("MONITORING_COMMANDLINE_TOOL")."</td>";
-print "<td><input type=\"text\" class=\"flat\" name=\"MONITORING_COMMANDLINE_TOOL\" value=\"". ($_POST["MONITORING_COMMANDLINE_TOOL"]?$_POST["MONITORING_COMMANDLINE_TOOL"]:$conf->global->MONITORING_COMMANDLINE_TOOL) . "\" size=\"50\"></td>";
-print "<td>/usr/bin/rrdtool";
-print "</td>";
-print "</tr>";
-
-print "</table>";
-
-print '<br><center>';
-print "<input type=\"submit\" name=\"save\" class=\"button\" value=\"".$langs->trans("Save")."\">";
-print "</center>";
-
-print "</form>\n";
-
-print '<br>';
-*/
-
 clearstatcache();
-
 
 print $langs->trans("ConcatPDfTakeFileFrom",$conf->concatpdf->dir_output.'/invoices');
 print '<br><br>';
@@ -140,5 +78,5 @@ print $langs->trans("ConcatPDfPutFileManually");
 
 $db->close();
 
-llxFooter('$Date: 2011/06/15 17:30:32 $ - $Revision: 1.2 $');
+llxFooter('$Date: 2011/08/10 10:21:23 $ - $Revision: 1.3 $');
 ?>
