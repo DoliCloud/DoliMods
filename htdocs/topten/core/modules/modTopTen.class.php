c869a20abdee5b8cd9dcc3490e473fb1f3078660
Laurent Destailleur <eldy@destailleur.fr>
2012-04-28 18:58:16 +0200

    Update logo

--- htdocs/topten/core/modules/modTopTen.class.php
+++ htdocs/topten/core/modules/modTopTen.class.php
@@ -42,7 +42,7 @@ class modTopTen extends DolibarrModules
 		// Name of image file used for this module.
 		// If file is in theme/yourtheme/img directory under name object_pictovalue.png, use this->picto='pictovalue'
 		// If file is in module/img directory under name object_pictovalue.png, use this->picto='pictovalue@module'
-		$this->picto='topten.png@topten';
+		$this->picto='topten@topten';
 
 		// Defined all module parts (triggers, login, substitutions, menus, css, etc...)
 		// for default path (eg: /mymodule/core/xxxxx) (0=disable, 1=enable)
