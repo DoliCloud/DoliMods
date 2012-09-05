be512a6c33d34009e3fc8b063af8abd256826ee0
Laurent Destailleur <eldy@destailleur.fr>
2012-09-05 10:49:17 +0200

    Fix: Add missing page

---
+++ htdocs/moretemplates/admin/moretemplates.php
@@ -0,0 +1,71 @@
+<?php
+/* Copyright (C) 2008-2011 Laurent Destailleur  <eldy@users.sourceforge.net>
+ */
+
+/**
+ *	    \file       htdocs/moretemplates/admin/moretemplates.php
+ *      \ingroup    moretemplates
+ *      \brief      Setup page for moretemplates module
+ */
+
+define('NOCSRFCHECK',1);
+
+$res=0;
+if (! $res && file_exists("../main.inc.php")) $res=@include("../main.inc.php");
+if (! $res && file_exists("../../main.inc.php")) $res=@include("../../main.inc.php");
+if (! $res && file_exists("../../../main.inc.php")) $res=@include("../../../main.inc.php");
+if (! $res && file_exists("../../../dolibarr/htdocs/main.inc.php")) $res=@include("../../../dolibarr/htdocs/main.inc.php");     // Used on dev env only
+if (! $res && file_exists("../../../../dolibarr/htdocs/main.inc.php")) $res=@include("../../../../dolibarr/htdocs/main.inc.php");   // Used on dev env only
+if (! $res && file_exists("../../../../../dolibarr/htdocs/main.inc.php")) $res=@include("../../../../../dolibarr/htdocs/main.inc.php");   // Used on dev env only
+if (! $res) die("Include of main fails");
+require_once(DOL_DOCUMENT_ROOT."/core/lib/admin.lib.php");
+require_once(DOL_DOCUMENT_ROOT."/core/lib/date.lib.php");
+require_once(DOL_DOCUMENT_ROOT.'/core/class/html.formadmin.class.php');
+require_once(DOL_DOCUMENT_ROOT.'/core/class/html.formother.class.php');
+
+if (!$user->admin) accessforbidden();
+
+$langs->load("moretemplates@moretemplates");
+$langs->load("admin");
+$langs->load("other");
+
+$def = array();
+$action=GETPOST("action");
+
+
+/*
+ * Actions
+ */
+
+// None
+
+
+
+/*
+ * View
+ */
+
+
+$form=new Form($db);
+$formadmin=new FormAdmin($db);
+$formother=new FormOther($db);
+
+$help_url='';
+//$arrayofjs=array('/includes/jquery/plugins/colorpicker/jquery.colorpicker.js');
+//$arrayofcss=array('/includes/jquery/plugins/colorpicker/jquery.colorpicker.css');
+$arrayofjs=array();
+$arrayofcss=array();
+llxHeader('',$langs->trans("MoreTemplateSetup"),$help_url,'',0,0,$arrayofjs,$arrayofcss);
+
+$linkback='<a href="'.DOL_URL_ROOT.'/admin/modules.php">'.$langs->trans("BackToModuleList").'</a>';
+print_fiche_titre($langs->trans("MoreTemplateSetup"),$linkback,'setup');
+print '<br>';
+
+
+print $langs->trans("ThisModuleHasMoreTemplatesIntoSomeModule").'<br>';
+
+
+llxFooter();
+
+$db->close();
+?>
