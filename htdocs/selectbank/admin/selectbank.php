a95a533dc403e57ff3d18c78dd4018d4113c2b48
Laurent Destailleur <eldy@destailleur.fr>
2013-02-23 15:47:35 +0100

    Add selectbank module

---
+++ htdocs/selectbank/admin/selectbank.php
@@ -0,0 +1,87 @@
+<?php
+/* Copyright (C) 2008-2011	Laurent Destailleur  <eldy@users.sourceforge.net>
+ * Copyright (C) 2012		Regis Houssin        <regis.houssin@capnetworks.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *	    \file       htdocs/concatpdf/admin/concatpdf.php
+ *      \ingroup    concatpdf
+ *      \brief      Page to setup module ConcatPdf
+ */
+
+define('NOCSRFCHECK',1);
+
+$res=0;
+if (! $res && file_exists("../main.inc.php")) $res=@include("../main.inc.php");
+if (! $res && file_exists("../../main.inc.php")) $res=@include("../../main.inc.php");
+if (! $res && file_exists("../../../main.inc.php")) $res=@include("../../../main.inc.php");
+
+// Use on dev env only
+if (! $res && file_exists($_SERVER['DOCUMENT_ROOT']."/main.inc.php")) $res=@include($_SERVER['DOCUMENT_ROOT']."/main.inc.php");
+
+if (! $res && file_exists("../../../dolibarr/htdocs/main.inc.php")) $res=@include("../../../dolibarr/htdocs/main.inc.php");     // Used on dev env only
+if (! $res && file_exists("../../../../dolibarr/htdocs/main.inc.php")) $res=@include("../../../../dolibarr/htdocs/main.inc.php");   // Used on dev env only
+if (! $res && file_exists("../../../../../dolibarr/htdocs/main.inc.php")) $res=@include("../../../../../dolibarr/htdocs/main.inc.php");   // Used on dev env only
+if (! $res) die("Include of main fails");
+require_once(DOL_DOCUMENT_ROOT."/core/lib/admin.lib.php");
+require_once(DOL_DOCUMENT_ROOT."/core/lib/files.lib.php");
+require_once(DOL_DOCUMENT_ROOT.'/core/class/html.formadmin.class.php');
+require_once(DOL_DOCUMENT_ROOT.'/core/class/html.formfile.class.php');
+
+
+if (!$user->admin) accessforbidden();
+
+$langs->load("admin");
+$langs->load("other");
+$langs->load("selectbank@selectbank");
+
+$def = array();
+$action=GETPOST('action', 'alpha');
+$confirm=GETPOST('confirm', 'alpha');
+$actionsave=GETPOST('save', 'alpha');
+
+$modules = array('proposals','orders','invoices');
+
+
+/*
+ * Actions
+ */
+
+
+/*
+ * View
+ */
+
+$form=new Form($db);
+$formfile=new FormFile($db);
+
+llxHeader('','SelectBank',$linktohelp);
+
+$linkback='<a href="'.DOL_URL_ROOT.'/admin/modules.php">'.$langs->trans("BackToModuleList").'</a>';
+print_fiche_titre($langs->trans("SelectBank"),$linkback,'setup');
+print '<br>';
+
+clearstatcache();
+
+dol_htmloutput_mesg($mesg,$mesgs);
+
+print 'Nothing to setup';
+
+// Footer
+llxFooter();
+// Close database handler
+$db->close();
+?>
