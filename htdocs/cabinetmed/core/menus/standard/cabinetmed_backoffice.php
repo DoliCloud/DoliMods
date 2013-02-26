6e5f2406e37480d717d6b9f3d8f922d247e8f1dd
Laurent Destailleur <eldy@destailleur.fr>
2013-02-26 22:30:02 +0100

    Resote menu for 3.3

--- htdocs/cabinetmed/core/menus/standard/cabinetmed_backoffice.php
+++ htdocs/cabinetmed/core/menus/standard/cabinetmed_backoffice.php
@@ -1,5 +1,6 @@
 <?php
-/* Copyright (C) 2005-2013 Laurent Destailleur  <eldy@users.sourceforge.net>
+/* Copyright (C) 2005-2010 Laurent Destailleur  <eldy@users.sourceforge.net>
+ * Copyright (C) 2007-2010 Regis Houssin        <regis@dolibarr.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,8 +17,9 @@
  */
 
 /**
- *		\file       htdocs/cabinetmed/core/menus/standard/cabinetmed_backoffice.php
+ *		\file       htdocs/core/menus/standard/cabinetmed_backoffice.php
  *		\brief      Gestionnaire nomme cabinetmed du menu du haut
+ *		\version    $Id: cabinetmed_backoffice.php,v 1.2 2011/02/13 15:49:43 eldy Exp $
  *
  *		\remarks    La construction d'un gestionnaire pour le menu du haut est simple:
  *		\remarks    Toutes les entrees de menu a faire apparaitre dans la barre du haut
@@ -27,24 +29,23 @@
 
 
 /**
- *  Classe to manage menu Eldy
+ *      \class      MenuTop
+ *	    \brief      Class to manage top menu cabinetmed (for internal users)
  */
-class MenuManager 
-{
-	var $db;
-	var $type_user=0;								// Put 0 for internal users, 1 for external users
-	var $atarget="";                                // Valeur du target a utiliser dans les liens
+class MenuTop {
 
-	var $menu_array;
-	var $menu_array_after;
+	var $require_left=array("cabinetmed_backoffice");     // Si doit etre en phase avec un gestionnaire de menu gauche particulier
+	var $hideifnotallowed=0;						// Put 0 for back office menu, 1 for front office menu
+	var $atarget="";                                // Valeur du target a utiliser dans les liens
 
 
 	/**
 	 *  Constructor
 	 *
 	 *  @param	DoliDB	$db		Database handler
+	 *  @return	void
 	 */
-	function __construct($db)
+	function MenuTop($db)
 	{
 		$this->db=$db;
 	}
@@ -53,19 +54,57 @@ class MenuManager
 	/**
 	 *  Show menu
 	 *
-	 *	@param	string	$mode		'top' or 'left'
-	 *  @return	int     			Number of menu entries shown
+	 *  @return	void
 	 */
-	function showmenu($mode)
+	function showmenu()
 	{
 		dol_include_once('/cabinetmed/core/menus/cabinetmed.lib.php');
-	
-		$res='ErrorBadParameterForMode';
-		if ($mode == 'top')  $res=print_cabinetmed_menu($this->db,$this->atarget,$this->type_user);
-		if ($mode == 'left') $res=print_left_cabinetmed_menu($this->db,$this->menu_array,$this->menu_array_after);
-	
-		return $res;
+
+		print_cabinetmed_menu($this->db,$this->atarget,$this->hideifnotallowed);
 	}
+
+}
+
+
+/**
+ *  \class      MenuLeft
+ *  \brief      Classe permettant la gestion du menu du gauche cabinetmed
+ */
+class MenuLeft {
+
+    var $require_top=array("cabinetmed_backoffice");     // Si doit etre en phase avec un gestionnaire de menu du haut particulier
+
+    var $db;
+    var $menu_array;
+    var $menu_array_after;
+
+
+    /**
+     *  Constructor
+     *  @param      db                  Database handler
+     *  @param      menu_array          Table of menu entries to show before entries of menu handler
+     *  @param      menu_array_after    Table of menu entries to show after entries of menu handler
+     */
+    function MenuLeft($db,&$menu_array,&$menu_array_after)
+    {
+        $this->db=$db;
+        $this->menu_array=$menu_array;
+        $this->menu_array_after=$menu_array_after;
+    }
+
+
+    /**
+     *      \brief      Show menu
+     *      \return     int     Number of menu entries shown
+     */
+    function showmenu()
+    {
+        dol_include_once('/cabinetmed/core/menus/cabinetmed.lib.php');
+
+        $res=print_left_cabinetmed_menu($this->db,$this->menu_array,$this->menu_array_after);
+
+        return $res;
+    }
 
 }
 
--- htdocs/cabinetmed/core/menus/standard/cabinetmed_frontoffice.php
+++ htdocs/cabinetmed/core/menus/standard/cabinetmed_frontoffice.php
@@ -1,5 +1,6 @@
 <?php
-/* Copyright (C) 2005-2013 Laurent Destailleur  <eldy@users.sourceforge.net>
+/* Copyright (C) 2005-2010 Laurent Destailleur  <eldy@users.sourceforge.net>
+ * Copyright (C) 2007-2009 Regis Houssin        <regis@dolibarr.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,8 +17,9 @@
  */
 
 /**
- *	\file       htdocs/cabinetmed/core/menus/standard/cabinetmed_frontoffice.php
+ *	\file       htdocs/core/menus/standard/cabinetmed_frontoffice.php
  *	\brief      Gestionnaire nomme cabinetmed du menu du haut
+ *	\version    $Id: cabinetmed_frontoffice.php,v 1.3 2011/02/13 15:49:43 eldy Exp $
  *
  *	\remarks    La construction d'un gestionnaire pour le menu du haut est simple:
  *	\remarks    Toutes les entrees de menu a faire apparaitre dans la barre du haut
@@ -27,24 +29,23 @@
 
 
 /**
- *	Class to manage menu Eldy (for external users)
+ *      \class      MenuTop
+ *	    \brief      Class to manage top menu cabinetmed (for external users)
  */
-class MenuManager
-{
-	var $db;
-	var $type_user=1;								// Put 0 for internal users, 1 for external users
-	var $atarget="";                                // Valeur du target a utiliser dans les liens
+class MenuTop {
 
-	var $menu_array;
-	var $menu_array_after;
+	var $require_left=array("cabinetmed_frontoffice");    // Si doit etre en phase avec un gestionnaire de menu gauche particulier
+	var $hideifnotallowed=1;						// Put 0 for back office menu, 1 for front office menu
+	var $atarget="";                                // Valeur du target a utiliser dans les liens
 
 
 	/**
 	 *  Constructor
 	 *
 	 *  @param	DoliDB	$db		Database handler
+	 *  @return	void
 	 */
-	function __construct($db)
+	function MenuTop($db)
 	{
 		$this->db=$db;
 	}
@@ -53,24 +54,60 @@ class MenuManager
 	/**
 	 *  Show menu
 	 *
-	 *	@param	string	$mode		'top' or 'left'
-	 *  @return int     			Number of menu entries shown
+	 *  @return	void
 	 */
-	function showmenu($mode)
+	function showmenu()
 	{
-		global $conf;
-
 		dol_include_once('/cabinetmed/core/menus/cabinetmed.lib.php');
 
-		$conf->global->MAIN_SEARCHFORM_SOCIETE=0;
-		$conf->global->MAIN_SEARCHFORM_CONTACT=0;
-
-		$res='ErrorBadParameterForMode';
-		if ($mode == 'top')  $res=print_cabinetmed_menu($this->db,$this->atarget,$this->type_user);
-		if ($mode == 'left') $res=print_left_cabinetmed_menu($this->db,$this->menu_array,$this->menu_array_after);
-
-		return $res;
+		print_cabinetmed_menu($this->db,$this->atarget,$this->hideifnotallowed);
 	}
+
+}
+
+
+/**
+ *  \class      MenuLeft
+ *  \brief      Classe permettant la gestion du menu du gauche cabinetmed
+ */
+class MenuLeft {
+
+    var $require_top=array("cabinetmed_frontoffice");     // Si doit etre en phase avec un gestionnaire de menu du haut particulier
+
+    var $db;
+    var $menu_array;
+    var $menu_array_after;
+
+
+    /**
+     *  Constructor
+     *  @param      db                  Database handler
+     *  @param      menu_array          Table of menu entries to show before entries of menu handler
+     *  @param      menu_array_after    Table of menu entries to show after entries of menu handler
+     */
+    function MenuLeft($db,&$menu_array,&$menu_array_after)
+    {
+        $this->db=$db;
+        $this->menu_array=$menu_array;
+        $this->menu_array_after=$menu_array_after;
+    }
+
+
+    /**
+     *      \brief      Show menu
+     *      \return     int     Number of menu entries shown
+     */
+    function showmenu()
+    {
+        dol_include_once('/cabinetmed/core/menus/cabinetmed.lib.php');
+
+        $res=print_left_cabinetmed_menu($this->db,$this->menu_array,$this->menu_array_after);
+
+        $conf->global->MAIN_SEARCHFORM_SOCIETE=0;
+        $conf->global->MAIN_SEARCHFORM_CONTACT=0;
+
+        return $res;
+    }
 
 }
 
