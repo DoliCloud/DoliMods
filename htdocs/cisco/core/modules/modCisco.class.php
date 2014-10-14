dff7bd4e124e0426a87aafa8323f32c9fcde61f9
Laurent Destailleur <eldy@destailleur.fr>
2014-10-14 16:20:43 +0200

    Fix: cisco number

--- htdocs/cisco/core/modules/modCisco.class.php
+++ htdocs/cisco/core/modules/modCisco.class.php
@@ -44,7 +44,7 @@ class modCisco extends DolibarrModules
 
         // Id for module (must be unique).
         // Use here a free id (See in Home -> System information -> Dolibarr for list of used modules id).
-        $this->numero = 101110;
+        $this->numero = 101130;
         // Key text used to identify module (for permissions, menus, etc...)
         $this->rights_class = 'cisco';
 
