817e1156ffc80617c1fb3369c6fa787aca903a07
Laurent Destailleur <eldy@destailleur.fr>
2014-11-04 00:46:41 +0100

    Gestion multiselect.

--- htdocs/partipirate/class/actions_partipirate.class.php
+++ htdocs/partipirate/class/actions_partipirate.class.php
@@ -113,8 +113,6 @@ class ActionsPartiPirate
         	{
         		$out.='</td></tr>';
 
-        		dol_include_once('/concatpdf/core/tpl/ajaxmultiselect.tpl.php');
-
         		$out.='<tr><td id="selectconcatpdf" colspan="4" valign="top">';
         		$out.= $form->multiselectarray('concatpdffile', $morefiles, (! empty($object->extraparams['concatpdf'])?$object->extraparams['concatpdf']:''), 0, 1, '', 1);
         	}
