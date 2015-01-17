0bb2a413fc1fc46112aa54bfa3cd3e7bb4874cfe
Laurent Destailleur <eldy@destailleur.fr>
2015-01-17 17:04:29 +0100

    Ajoute info path

--- htdocs/nltechno/dolicloud/dolicloud_card_upgrade_new.php
+++ htdocs/nltechno/dolicloud/dolicloud_card_upgrade_new.php
@@ -189,7 +189,8 @@ if (($id > 0 || $instance) && $action != 'edit' && $action != 'create')
 	print '<table class="border" width="100%">';
 
 	// SFTP
-	print '<tr><td width="20%">'.$langs->trans("SFTP Server").'</td><td colspan="3">'.$object->hostname_web.'</td>';
+	print '<tr><td width="20%">'.$langs->trans("SFTP Server").'</td><td>'.$object->hostname_web.'</td>';
+	print '<td>'.$langs->trans("FsPath").'</td><td>'.$object->fs_path.'</td>';
 	print '</tr>';
 	// Login/Pass
 	print '<tr>';
@@ -336,14 +337,14 @@ if (($id > 0 || $instance) && $action != 'edit' && $action != 'create')
 
 	// Document restore
 	$sftprestorestring='rsync -n -v -a dolibarr_documents/* '.$object->username_web.'@'.$object->hostname_web.':'.$object->fs_path.'/documents';
-	print 'Rsync overwrite document dir (supprimer le -n pour exécuter réellement):<br>';
+	print 'Rsync to copy/overwrite document dir (remove -n to execute really):<br>';
 	print '<input type="text" name="sftprestorestring" value="'.$sftprestorestring.'" size="160"><br>';
 
 	print '<br>';
 
 	// Deploy module
-	$sftpdeploystring='rsync -n -v -a dirmodulehtdocs/* '.$object->username_web.'@'.$object->hostname_web.':'.$object->fs_path.'/htdocs';
-	print 'Rsync overwrite module (supprimer le -n pour exécuter réellement):<br>';
+	$sftpdeploystring='rsync -n -v -a pathtohtdocsmodule/* '.$object->username_web.'@'.$object->hostname_web.':'.$object->fs_path.'/htdocs/namemodule';
+	print 'Rsync to install or overwrite module (remove -n to execute really):<br>';
 	print '<input type="text" name="sftpdeploystring" value="'.$sftpdeploystring.'" size="160"><br>';
 
 	print '<br>';
