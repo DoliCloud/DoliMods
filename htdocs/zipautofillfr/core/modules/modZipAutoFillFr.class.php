85c5005ddf349883b7aaea5e7dcd5c6724ed074f
Laurent Destailleur <eldy@destailleur.fr>
2015-06-21 00:50:54 +0200

    Compatibility with 3.7 (table llx_c_country instead of llx_c_pays)

--- htdocs/zipautofillfr/core/modules/modZipAutoFillFr.class.php
+++ htdocs/zipautofillfr/core/modules/modZipAutoFillFr.class.php
@@ -56,7 +56,7 @@ class modZipAutoFillFr extends DolibarrModules
 		$this->description = "Add zip codes and towns into database (France and Belgium)";
 
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
-		$this->version = '3.4';
+		$this->version = '3.7';
 		$this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
 		$this->special = 2;
 		$this->picto='generic';
@@ -66,6 +66,7 @@ class modZipAutoFillFr extends DolibarrModules
 
 		// Dependances
 		$this->depends = array();
+		$this->need_dolibarr_version = array(3,7,-3);   // Minimum version of Dolibarr required by module
 		$this->langfiles = array('zipautofillfr@zipautofillfr');
 
 		// Config pages
@@ -83,7 +84,7 @@ class modZipAutoFillFr extends DolibarrModules
             'langs'=>'',
             'tabname'=>array(MAIN_DB_PREFIX."c_ziptown"),
             'tablib'=>array("Zip and town"),
-            'tabsql'=>array('SELECT f.rowid, f.zip, f.town, f.fk_pays, p.code as pays_code, p.libelle as pays, f.active FROM '.MAIN_DB_PREFIX.'c_ziptown as f, '.MAIN_DB_PREFIX.'c_pays as p WHERE f.fk_pays=p.rowid'),
+            'tabsql'=>array('SELECT f.rowid, f.zip, f.town, f.fk_pays, p.code as pays_code, p.libelle as pays, f.active FROM '.MAIN_DB_PREFIX.'c_ziptown as f, '.MAIN_DB_PREFIX.'c_country as p WHERE f.fk_pays=p.rowid'),
             'tabsqlsort'=>array("zip ASC, town ASC"),
             'tabfield'=>array("zip,town"),
             'tabfieldvalue'=>array("zip,town"),
