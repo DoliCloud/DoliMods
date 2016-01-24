1d24eff669b5a48079308f188ed14a97ecb9a03f
Laurent Destailleur <eldy@destailleur.fr>
2016-01-24 19:11:15 +0100

    Module OVH now use the new APIv6

--- htdocs/ovh/core/modules/modOvh.class.php
+++ htdocs/ovh/core/modules/modOvh.class.php
@@ -56,7 +56,7 @@ class modOvh extends DolibarrModules
 		// Module description, used if translation string 'ModuleXXXDesc' not found (where XXX is value of numeric property 'numero' of module)
 		$this->description = "Add some features to use OVH interfaces (Send SMS with a subscription to OVH SMS API and make Click2Dial with OVH SIP server)";
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
-		$this->version = '3.7.0';
+		$this->version = '3.8.0';
 		// Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
 		$this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
 		// Where to store the module in setup page (0=common,1=interface,2=others,3=very specific)
