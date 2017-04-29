670f20b1896c2e8d7e1d210cb2617f9221b5e10b
Laurent Destailleur <eldy@destailleur.fr>
2017-04-29 20:11:39 +0200

    Fix compatibility with dolibarr 5.0

--- htdocs/ecotaxdeee/core/modules/modEcoTaxDeee.class.php
+++ htdocs/ecotaxdeee/core/modules/modEcoTaxDeee.class.php
@@ -134,12 +134,16 @@ class modEcoTaxDeee extends DolibarrModules
 	}
 
 	/**
-	 *		\brief      Function called when module is enabled.
-	 *					The init function add constants, boxes, permissions and menus (defined in constructor) into Dolibarr database.
-	 *					It also creates data directories.
-	 *      \return     int             1 if OK, 0 if KO
+	 * Function called when module is enabled.
+	 * The init function adds tabs, constants, boxes, permissions and menus (defined in constructor) into Dolibarr database.
+	 * It also creates data directories
+	 *
+	 * @param string $options   Options when enabling module ('', 'newboxdefonly', 'noboxes')
+     *                          'noboxes' = Do not insert boxes
+     *                          'newboxdefonly' = For boxes, insert def of boxes only and not boxes activation
+	 * @return int				1 if OK, 0 if KO
 	 */
-	function init()
+	function init($options = '')
 	{
 		global $langs;
 
@@ -159,20 +163,22 @@ class modEcoTaxDeee extends DolibarrModules
 
 		$result=$this->load_tables();
 
-		return $this->_init($sql);
+		return $this->_init($sql, $options);
 	}
 
 	/**
-	 *		\brief		Function called when module is disabled.
-	 *              	Remove from database constants, boxes and permissions from Dolibarr database.
-	 *					Data directories are not deleted.
-	 *      \return     int             1 if OK, 0 if KO
+	 * Function called when module is disabled.
+	 * The remove function removes tabs, constants, boxes, permissions and menus from Dolibarr database.
+	 * Data directories are not deleted
+	 *
+	 * @param      string	$options    Options when enabling module ('', 'noboxes')
+	 * @return     int             		1 if OK, 0 if KO
 	 */
-	function remove()
+	function remove($options = '')
 	{
 		$sql = array();
 
-		return $this->_remove($sql);
+		return $this->_remove($sql, $options);
 	}
 
 
