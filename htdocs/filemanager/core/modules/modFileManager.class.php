29eb78bd7e7bab3b0572da407126d6fdf679ee96
Laurent Destailleur <eldy@destailleur.fr>
2017-07-08 14:54:34 +0200

    Fix min version of dolibarr (3.6)

--- htdocs/filemanager/core/modules/modFileManager.class.php
+++ htdocs/filemanager/core/modules/modFileManager.class.php
@@ -81,8 +81,8 @@ class modFileManager extends DolibarrModules
 		//-------------
 		$this->depends = array();
 		$this->requiredby = array();
-        $this->phpmin = array(4,1);                 // Minimum version of PHP required by module
-        $this->need_dolibarr_version = array(6,0,-2);  // Minimum version of Dolibarr required by module
+        $this->phpmin = array(4,1);                    // Minimum version of PHP required by module
+        $this->need_dolibarr_version = array(3,6,-2);  // Minimum version of Dolibarr required by module
 		$this->langfiles = array("companies","filemanager@filemanager");
 
 		// Constantes
