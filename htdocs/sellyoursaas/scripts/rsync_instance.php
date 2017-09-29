9defd437c3c8f54f225c8be62697c83986e67402
Laurent Destailleur <eldy@destailleur.fr>
2017-09-30 01:59:49 +0200

    Add option clean

--- htdocs/sellyoursaas/scripts/rsync_instance.php
+++ htdocs/sellyoursaas/scripts/rsync_instance.php
@@ -72,7 +72,7 @@ $object = new Dolicloud_customers($db,$db2);
 if (empty($dirroot) || empty($instance) || empty($mode))
 {
 	print "Update an instance on stratus5 server with new ref version.\n";
-	print "Usage: $script_file dolibarr_root_dir dolicloud_instance (test|confirm|confirmunlock|diff|diffadd|diffchange)\n";
+	print "Usage: $script_file dolibarr_root_dir dolicloud_instance (test|confirm|confirmunlock|diff|diffadd|diffchange|clean|confirmclean)\n";
 	print "Return code: 0 if success, <>0 if error\n";
 	exit(-1);
 }
@@ -110,7 +110,7 @@ print 'SFTP password '.$object->password_web."\n";
 
 $command="rsync";
 $param=array();
-if (! in_array($mode,array('confirm','confirmunlock'))) $param[]="-n";
+if (! in_array($mode,array('confirm','confirmunlock','confirmclean'))) $param[]="-n";
 //$param[]="-a";
 if (! in_array($mode,array('diff','diffadd','diffchange'))) $param[]="-rlt";
 else { $param[]="-rlD"; $param[]="--modify-window=1000000000"; $param[]="--delete -n"; }
@@ -129,7 +129,11 @@ $param[]="--exclude htdocs/conf/conf.php*";
 $param[]="--exclude htdocs/custom";
 $param[]="--exclude htdocs/customfields/";
 $param[]="--exclude htdocs/bootstrap/";
+$param[]="--exclude htdocs/agefodd/";
+$param[]="--exclude htdocs/memcached/";
+$param[]="--exclude htdocs/cabinetmed/";
 if (! in_array($mode,array('diff','diffadd','diffchange'))) $param[]="--stats";
+if (! in_array($mode,array('clean','confirmclean'))) $param[]="--delete";
 $param[]="-e 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'";
 
 $param[]=$dirroot.'/';
