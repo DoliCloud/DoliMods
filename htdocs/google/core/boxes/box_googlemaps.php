58b57753ae399e52c8af2c162cf83cb7eef94c76
Laurent Destailleur <eldy@destailleur.fr>
2017-10-02 14:45:23 +0200

    Compatibility v6

--- htdocs/google/core/boxes/box_googlemaps.php
+++ htdocs/google/core/boxes/box_googlemaps.php
@@ -156,11 +156,12 @@ class box_googlemaps extends ModeleBoxes
 	 *
 	 *	@param	array	$head       Array with properties of box title
 	 *	@param  array	$contents   Array with properties of box lines
+	 *  @param	int		$nooutput	No print, only return string
 	 *	@return	void
 	 */
-	function showBox($head = null, $contents = null)
+	function showBox($head = null, $contents = null, $nooutput = 0)
 	{
-		parent::showBox($this->info_box_head, $this->info_box_contents);
+		return parent::showBox($this->info_box_head, $this->info_box_contents, $nooutput);
 	}
 
 }
