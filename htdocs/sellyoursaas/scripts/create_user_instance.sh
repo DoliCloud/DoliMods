56244d5c432efc42810eabe8ea58070cfc9660b2
Laurent Destailleur <eldy@destailleur.fr>
2017-11-03 02:36:05 +0100

    Check params

--- htdocs/sellyoursaas/scripts/create_user_instance.sh
+++ htdocs/sellyoursaas/scripts/create_user_instance.sh
@@ -35,9 +35,11 @@ fi
 
 if [ "x$1" == "x" ]; then
 	echo "Missing parameter 1 - osusername" 1>&2
+	exit 1
 fi
 if [ "x$2" == "x" ]; then
 	echo "Missing parameter 2 - instancename" 1>&2
+	exit 1
 fi
 
 export osusername=$1
