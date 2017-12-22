4bdfff0496731ea1b80911fb786f8ae1635025ab
Laurent Destailleur <eldy@destailleur.fr>
2017-12-22 15:11:03 +0100

    Log in append

--- htdocs/sellyoursaas/public/spamreport.php
+++ htdocs/sellyoursaas/public/spamreport.php
@@ -8,7 +8,7 @@
 $tmpfile = '/tmp/spamreport.log';
 $date = strftime("%Y-%m-%d %H:%M:%S" ,time());
 
-file_put_contents($tmpfile, "***** Spam report received ".$date."*****\n", FILE_APPEND);
+file_put_contents($tmpfile, "\n***** Spam report received ".$date."*****\n", FILE_APPEND);
 file_put_contents($tmpfile, var_export($_SERVER, true), FILE_APPEND);
 file_put_contents($tmpfile, var_export($_GET, true), FILE_APPEND);
 file_put_contents($tmpfile, var_export($_POST, true), FILE_APPEND);
