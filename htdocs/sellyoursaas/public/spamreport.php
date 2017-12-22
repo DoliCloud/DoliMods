de45062c486850620d4d014553d727c162e65076
Laurent Destailleur <eldy@destailleur.fr>
2017-12-22 15:25:47 +0100

    Try to send email

--- htdocs/sellyoursaas/public/spamreport.php
+++ htdocs/sellyoursaas/public/spamreport.php
@@ -2,11 +2,13 @@
 /*
  * Page to report spam
  *
- * This page is a service called by SendGrid to report a SPAMMER.
+ * This page is a service called by SendGrid or other services to report a SPAM.
  */
 
 $tmpfile = '/tmp/spamreport.log';
 $date = strftime("%Y-%m-%d %H:%M:%S" ,time());
+
+echo "Spam report received at ".$date;
 
 file_put_contents($tmpfile, "\n***** Spam report received ".$date."*****\n", FILE_APPEND);
 file_put_contents($tmpfile, var_export($_SERVER, true), FILE_APPEND);
@@ -14,5 +16,5 @@ file_put_contents($tmpfile, var_export($_GET, true), FILE_APPEND);
 file_put_contents($tmpfile, var_export($_POST, true), FILE_APPEND);
 file_put_contents("\n", FILE_APPEND);
 
-echo "Spam report received at ".$date;
+mail('supervision@nltechno.com', 'Spam report recevied from SendGrid', 'Spam was reported by sendgrid');
 
