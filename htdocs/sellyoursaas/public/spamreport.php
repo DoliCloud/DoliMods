b8c77eff39632b577ef0f9169af77bad760761ac
Laurent Destailleur <eldy@destailleur.fr>
2017-12-22 15:08:33 +0100

    Add date

--- htdocs/sellyoursaas/public/spamreport.php
+++ htdocs/sellyoursaas/public/spamreport.php
@@ -5,12 +5,13 @@
  * This page is a service called by SendGrid to report a SPAMMER.
  */
 
-$tmpfile = '/home/admin/logs/spamreport.log';
+$tmpfile = '/tmp/spamreport.log';
+$date = strftime("%Y-%m-%d %H:%M:%S" ,time());
 
-file_put_contents($tmpfile, "***** Spam report received *****\n");
+file_put_contents($tmpfile, "***** Spam report received ".$date."*****\n");
 file_put_contents($tmpfile, var_export($_SERVER, true), FILE_APPEND);
 file_put_contents($tmpfile, var_export($_GET, true), FILE_APPEND);
 file_put_contents($tmpfile, var_export($_POST, true), FILE_APPEND);
 
-echo "Spam report received";
+echo "Spam report received at ".$date;
 
