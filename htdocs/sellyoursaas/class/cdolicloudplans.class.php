fb6230eea134e88031192b56075b5537f3f27a08
Laurent Destailleur <eldy@destailleur.fr>
2018-04-17 22:01:41 +0200

    sys

---
+++ htdocs/sellyoursaas/class/cdolicloudplans.class.php
@@ -0,0 +1,429 @@
+<?php
+/* Copyright (C) 2007-2012 Laurent Destailleur  <eldy@users.sourceforge.net>
+ * Copyright (C) ---Put here your own copyright and developer email---
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *  \file       htdocs/sellyoursaas/class/cdolicloudplans.class.php
+ *  \ingroup    mymodule othermodule1 othermodule2
+ *  \brief      This file is an example for a CRUD class file (Create/Read/Update/Delete)
+ *				Initialy built by build_class_from_table on 2012-10-04 21:43
+ */
+
+// Put here all includes required by your class file
+//require_once(DOL_DOCUMENT_ROOT."/core/class/commonobject.class.php");
+//require_once(DOL_DOCUMENT_ROOT."/societe/class/societe.class.php");
+//require_once(DOL_DOCUMENT_ROOT."/product/class/product.class.php");
+
+
+/**
+ *	Put here description of your class
+ */
+class Cdolicloudplans // extends CommonObject
+{
+	var $db;							//!< To store db handler
+	var $error;							//!< To return error code (or message)
+	var $errors=array();				//!< To return several error codes (or messages)
+	//var $element='cdolicloudplans';			//!< Id that identify managed objects
+	//var $table_element='cdolicloudplans';	//!< Name of table without prefix where object is stored
+
+    var $id;
+
+	var $code;
+	var $label;
+	var $price_instance;
+	var $price_user;
+	var $price_gb;
+	var $active;
+
+
+
+
+    /**
+     *  Constructor
+     *
+     *  @param	DoliDb		$db      Database handler
+     */
+    function __construct($db)
+    {
+        $this->db = $db;
+        return 1;
+    }
+
+
+    /**
+     *  Create object into database
+     *
+     *  @param	User	$user        User that creates
+     *  @param  int		$notrigger   0=launch triggers after, 1=disable triggers
+     *  @return int      		   	 <0 if KO, Id of created object if OK
+     */
+    function create($user, $notrigger=0)
+    {
+    	global $conf, $langs;
+		$error=0;
+
+		// Clean parameters
+
+		if (isset($this->code)) $this->code=trim($this->code);
+		if (isset($this->label)) $this->label=trim($this->label);
+		if (isset($this->price_instance)) $this->price_instance=trim($this->price_instance);
+		if (isset($this->price_user)) $this->price_user=trim($this->price_user);
+		if (isset($this->price_gb)) $this->price_gb=trim($this->price_gb);
+		if (isset($this->active)) $this->active=trim($this->active);
+
+
+
+		// Check parameters
+		// Put here code to add control on parameters values
+
+        // Insert request
+		$sql = "INSERT INTO ".MAIN_DB_PREFIX."c_dolicloud_plans(";
+
+		$sql.= "code,";
+		$sql.= "label,";
+		$sql.= "price_instance,";
+		$sql.= "price_user,";
+		$sql.= "price_gb,";
+		$sql.= "active";
+
+
+        $sql.= ") VALUES (";
+
+		$sql.= " ".(! isset($this->code)?'NULL':"'".$this->db->escape($this->code)."'").",";
+		$sql.= " ".(! isset($this->label)?'NULL':"'".$this->db->escape($this->label)."'").",";
+		$sql.= " ".(! isset($this->price_instance)?'NULL':"'".$this->price_instance."'").",";
+		$sql.= " ".(! isset($this->price_user)?'NULL':"'".$this->price_user."'").",";
+		$sql.= " ".(! isset($this->price_gb)?'NULL':"'".$this->price_gb."'").",";
+		$sql.= " ".(! isset($this->active)?'NULL':"'".$this->active."'")."";
+
+
+		$sql.= ")";
+
+		$this->db->begin();
+
+	   	dol_syslog(get_class($this)."::create sql=".$sql, LOG_DEBUG);
+        $resql=$this->db->query($sql);
+    	if (! $resql) { $error++; $this->errors[]="Error ".$this->db->lasterror(); }
+
+		if (! $error)
+        {
+            $this->id = $this->db->last_insert_id(MAIN_DB_PREFIX."c_dolicloud_plans");
+
+			if (! $notrigger)
+			{
+	            // Uncomment this and change MYOBJECT to your own tag if you
+	            // want this action calls a trigger.
+
+	            //// Call triggers
+	            //include_once DOL_DOCUMENT_ROOT . '/core/class/interfaces.class.php';
+	            //$interface=new Interfaces($this->db);
+	            //$result=$interface->run_triggers('MYOBJECT_CREATE',$this,$user,$langs,$conf);
+	            //if ($result < 0) { $error++; $this->errors=$interface->errors; }
+	            //// End call triggers
+			}
+        }
+
+        // Commit or rollback
+        if ($error)
+		{
+			foreach($this->errors as $errmsg)
+			{
+	            dol_syslog(get_class($this)."::create ".$errmsg, LOG_ERR);
+	            $this->error.=($this->error?', '.$errmsg:$errmsg);
+			}
+			$this->db->rollback();
+			return -1*$error;
+		}
+		else
+		{
+			$this->db->commit();
+            return $this->id;
+		}
+    }
+
+
+    /**
+     *  Load object in memory from the database
+     *
+     *  @param	int		$id    	Id object
+     *  @param	string	$ref	Ref object
+     *  @return int          	<0 if KO, >0 if OK
+     */
+    function fetch($id,$ref='')
+    {
+    	global $langs;
+        $sql = "SELECT";
+		$sql.= " t.rowid,";
+
+		$sql.= " t.code,";
+		$sql.= " t.label,";
+		$sql.= " t.price_instance,";
+		$sql.= " t.price_user,";
+		$sql.= " t.price_gb,";
+		$sql.= " t.active";
+
+        $sql.= " FROM ".MAIN_DB_PREFIX."c_dolicloud_plans as t";
+		if (! empty($ref)) $sql.= " WHERE t.code = '".$this->db->escape($ref)."'";
+        else $sql.= " WHERE t.rowid = ".$id;
+
+    	dol_syslog(get_class($this)."::fetch sql=".$sql, LOG_DEBUG);
+        $resql=$this->db->query($sql);
+        if ($resql)
+        {
+            if ($this->db->num_rows($resql))
+            {
+                $obj = $this->db->fetch_object($resql);
+
+                $this->id    = $obj->rowid;
+
+				$this->code = $obj->code;
+				$this->label = $obj->label;
+				$this->price_instance = $obj->price_instance;
+				$this->price_user = $obj->price_user;
+				$this->price_gb = $obj->price_gb;
+				$this->active = $obj->active;
+
+
+            }
+            $this->db->free($resql);
+
+            return 1;
+        }
+        else
+        {
+      	    $this->error="Error ".$this->db->lasterror();
+            dol_syslog(get_class($this)."::fetch ".$this->error, LOG_ERR);
+            return -1;
+        }
+    }
+
+
+    /**
+     *  Update object into database
+     *
+     *  @param	User	$user        User that modifies
+     *  @param  int		$notrigger	 0=launch triggers after, 1=disable triggers
+     *  @return int     		   	 <0 if KO, >0 if OK
+     */
+    function update($user=null, $notrigger=0)
+    {
+    	global $conf, $langs;
+		$error=0;
+
+		// Clean parameters
+
+		if (isset($this->code)) $this->code=trim($this->code);
+		if (isset($this->label)) $this->label=trim($this->label);
+		if (isset($this->price_instance)) $this->price_instance=trim($this->price_instance);
+		if (isset($this->price_user)) $this->price_user=trim($this->price_user);
+		if (isset($this->price_gb)) $this->price_gb=trim($this->price_gb);
+		if (isset($this->active)) $this->active=trim($this->active);
+
+
+
+		// Check parameters
+		// Put here code to add a control on parameters values
+
+        // Update request
+        $sql = "UPDATE ".MAIN_DB_PREFIX."c_dolicloud_plans SET";
+
+		$sql.= " code=".(isset($this->code)?"'".$this->db->escape($this->code)."'":"null").",";
+		$sql.= " label=".(isset($this->label)?"'".$this->db->escape($this->label)."'":"null").",";
+		$sql.= " price_instance=".(isset($this->price_instance)?$this->price_instance:"null").",";
+		$sql.= " price_user=".(isset($this->price_user)?$this->price_user:"null").",";
+		$sql.= " price_gb=".(isset($this->price_gb)?$this->price_gb:"null").",";
+		$sql.= " active=".(isset($this->active)?$this->active:"null")."";
+
+
+        $sql.= " WHERE rowid=".$this->id;
+
+		$this->db->begin();
+
+		dol_syslog(get_class($this)."::update sql=".$sql, LOG_DEBUG);
+        $resql = $this->db->query($sql);
+    	if (! $resql) { $error++; $this->errors[]="Error ".$this->db->lasterror(); }
+
+		if (! $error)
+		{
+			if (! $notrigger)
+			{
+	            // Uncomment this and change MYOBJECT to your own tag if you
+	            // want this action calls a trigger.
+
+	            //// Call triggers
+	            //include_once DOL_DOCUMENT_ROOT . '/core/class/interfaces.class.php';
+	            //$interface=new Interfaces($this->db);
+	            //$result=$interface->run_triggers('MYOBJECT_MODIFY',$this,$user,$langs,$conf);
+	            //if ($result < 0) { $error++; $this->errors=$interface->errors; }
+	            //// End call triggers
+	    	}
+		}
+
+        // Commit or rollback
+		if ($error)
+		{
+			foreach($this->errors as $errmsg)
+			{
+	            dol_syslog(get_class($this)."::update ".$errmsg, LOG_ERR);
+	            $this->error.=($this->error?', '.$errmsg:$errmsg);
+			}
+			$this->db->rollback();
+			return -1*$error;
+		}
+		else
+		{
+			$this->db->commit();
+			return 1;
+		}
+    }
+
+
+ 	/**
+	 *  Delete object in database
+	 *
+     *	@param  User	$user        User that deletes
+     *  @param  int		$notrigger	 0=launch triggers after, 1=disable triggers
+	 *  @return	int					 <0 if KO, >0 if OK
+	 */
+	function delete($user, $notrigger=0)
+	{
+		global $conf, $langs;
+		$error=0;
+
+		$this->db->begin();
+
+		if (! $error)
+		{
+			if (! $notrigger)
+			{
+				// Uncomment this and change MYOBJECT to your own tag if you
+		        // want this action calls a trigger.
+
+		        //// Call triggers
+		        //include_once DOL_DOCUMENT_ROOT . '/core/class/interfaces.class.php';
+		        //$interface=new Interfaces($this->db);
+		        //$result=$interface->run_triggers('MYOBJECT_DELETE',$this,$user,$langs,$conf);
+		        //if ($result < 0) { $error++; $this->errors=$interface->errors; }
+		        //// End call triggers
+			}
+		}
+
+		if (! $error)
+		{
+    		$sql = "DELETE FROM ".MAIN_DB_PREFIX."c_dolicloud_plans";
+    		$sql.= " WHERE rowid=".$this->id;
+
+    		dol_syslog(get_class($this)."::delete sql=".$sql);
+    		$resql = $this->db->query($sql);
+        	if (! $resql) { $error++; $this->errors[]="Error ".$this->db->lasterror(); }
+		}
+
+        // Commit or rollback
+		if ($error)
+		{
+			foreach($this->errors as $errmsg)
+			{
+	            dol_syslog(get_class($this)."::delete ".$errmsg, LOG_ERR);
+	            $this->error.=($this->error?', '.$errmsg:$errmsg);
+			}
+			$this->db->rollback();
+			return -1*$error;
+		}
+		else
+		{
+			$this->db->commit();
+			return 1;
+		}
+	}
+
+
+
+	/**
+	 *	Load an object from its id and create a new one in database
+	 *
+	 *	@param	int		$fromid     Id of object to clone
+	 * 	@return	int					New id of clone
+	 */
+	function createFromClone($fromid)
+	{
+		global $user,$langs;
+
+		$error=0;
+
+		$object=new Cdolicloudplans($this->db);
+
+		$this->db->begin();
+
+		// Load source object
+		$object->fetch($fromid);
+		$object->id=0;
+		$object->statut=0;
+
+		// Clear fields
+		// ...
+
+		// Create clone
+		$result=$object->create($user);
+
+		// Other options
+		if ($result < 0)
+		{
+			$this->error=$object->error;
+			$error++;
+		}
+
+		if (! $error)
+		{
+
+
+		}
+
+		// End
+		if (! $error)
+		{
+			$this->db->commit();
+			return $object->id;
+		}
+		else
+		{
+			$this->db->rollback();
+			return -1;
+		}
+	}
+
+
+	/**
+	 *	Initialise object with example values
+	 *	Id must be 0 if object instance is a specimen
+	 *
+	 *	@return	void
+	 */
+	function initAsSpecimen()
+	{
+		$this->id=0;
+
+		$this->code='';
+		$this->label='';
+		$this->price_instance='';
+		$this->price_user='';
+		$this->price_gb='';
+		$this->active='';
+
+
+	}
+
+}
