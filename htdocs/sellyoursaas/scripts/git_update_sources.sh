f09c616281d83db69e2887930fd2ca3b716b0258
Laurent Destailleur <eldy@destailleur.fr>
2018-05-05 23:29:03 +0200

    sys

--- htdocs/sellyoursaas/scripts/git_update_sources.sh
+++ htdocs/sellyoursaas/scripts/git_update_sources.sh
@@ -7,7 +7,8 @@
 #---------------------------------------------------------
 
 if [ "x$1" == "x" ]; then
-   echo "Usage: $0  dir_documents"
+   echo "Usage:   $0  dir_document_of_git_repositories"
+   echo "Example: $0  /pathtodocuments/documents/sellyoursaas/git"
    exit 1
 fi
 
@@ -16,11 +17,17 @@ echo "Update git dirs found into $1"
 #for dir in `find $1 -type d`
 for dir in `ls -d $1/*`
 do
-	echo -- Process dir $dir
-	cd $dir
-	git pull --ff-only --depth=10
-	echo Result of git pull -ff-only = $?
-	cd -
+        echo -- Process dir $dir
+        cd $dir
+        git pull --ff-only --depth=10
+        echo Result of git pull -ff-only = $?
+
+        if [ -s build/generate_filelist_xml.php ]; then
+                echo "Found generate_filelist_xml.php"
+                php build/generate_filelist_xml.php release=auto-dolicloud
+        fi
+
+        cd -
 done
 
 echo "Finished."
