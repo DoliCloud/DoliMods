b42a521c2c5059158098286c8ac75e902545da28
Laurent Destailleur <eldy@destailleur.fr>
2018-06-19 13:20:02 +0200

    Fix men unot visible on jmobile

--- htdocs/phpsysinfo/core/modules/modPHPSysInfo.class.php
+++ htdocs/phpsysinfo/core/modules/modPHPSysInfo.class.php
@@ -139,7 +139,7 @@ class modPHPSysInfo extends DolibarrModules
 									'url'=>'/phpsysinfo/index.php',
 									'langs'=>'phpsysinfo@phpsysinfo',	// Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
 									'position'=>100,
-									'enabled'=>'$conf->phpsysinfo->enabled && preg_match(\'/^admintools/\', $leftmenu)',			// Define condition to show or hide menu entry. Use '$conf->phpsysinfo->enabled' if entry must be visible if module is enabled. Use '$leftmenu==\'system\'' to show if leftmenu system is selected.
+									'enabled'=>'$conf->phpsysinfo->enabled && preg_match(\'/^(admintools|all)/\',$leftmenu)',			// Define condition to show or hide menu entry. Use '$conf->phpsysinfo->enabled' if entry must be visible if module is enabled. Use '$leftmenu==\'system\'' to show if leftmenu system is selected.
 									'perms'=>'1',			// Use 'perms'=>'$user->rights->phpsysinfo->level1->level2' if you want your menu with a permission rules
 									'target'=>'',
 									'user'=>2);				// 0=Menu for internal users, 1=external users, 2=both
