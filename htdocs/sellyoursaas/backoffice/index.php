ecdc652350fe485d907ca1bd317301bd1765e588
Laurent Destailleur <eldy@destailleur.fr>
2018-07-01 00:39:09 +0200

    Fix trans

--- htdocs/sellyoursaas/backoffice/index.php
+++ htdocs/sellyoursaas/backoffice/index.php
@@ -173,7 +173,7 @@ print '</td><td align="right">';
 print '<font size="+2">'.$totalresellers.'</font>';
 print '</td></tr>';
 print '<tr class="oddeven"><td class="wordwrap wordbreak">';
-print $langs->trans("NbOfInstancesActivePaying").' / '.$langs->trans("NbOfInstancesPaying").' ';
+print $langs->trans("NbOfInstancesActivePaying").' / '.$langs->trans("NbOfActiveInstances").' ';
 print '</td><td align="right">';
 print '<font size="+2">'.$totalinstancespaying.' / '.$totalinstances.'</font>';
 print '</td></tr>';
