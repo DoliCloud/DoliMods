3737d159d3aa3dd63664a2364769f3b0bcad3b8e
Laurent Destailleur <eldy@destailleur.fr>
2018-09-03 10:00:45 +0200

    Remove log

--- htdocs/sellyoursaas/scripts/action_deploy_undeploy.sh
+++ htdocs/sellyoursaas/scripts/action_deploy_undeploy.sh
@@ -223,6 +223,9 @@ if [[ "$mode" == "undeployall" ]]; then
 
 	echo `date +%Y%m%d%H%M%S`" ***** Delete user $osusername with home into /home/jail/home/$osusername and archive it into $archivedir"
 	
+	echo rm -f /home/jail/home/$osusername/$dbname/*.log
+	rm -f /home/jail/home/$osusername/$dbname/*.log >/dev/null 2>&1 
+	
 	echo deluser --remove-home --backup --backup-to $archivedir $osusername
 	if [[ $testorconfirm == "confirm" ]]
 	then
