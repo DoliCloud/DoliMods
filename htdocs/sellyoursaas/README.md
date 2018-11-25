306c325a71ea936a40b908b167e37e2a378c6dd8
Laurent Destailleur <eldy@destailleur.fr>
2018-11-25 13:27:46 +0100

    Update migration scripts

--- htdocs/sellyoursaas/README.md
+++ htdocs/sellyoursaas/README.md
@@ -2,31 +2,31 @@
 
 
 ## Features
+
 SellYourSaas is a module to complete your ERP CRM so it is able to manage and sell Saas application on line.
 It covers definition of packages to sell, deployement of application on a remote server, a customer dashboard for
 your subscribers and automatic invoicing and renewal.
 
 This is a list of some features supported by this application:
 
-- Can create profiles of packages to define what to deploy when a subscription to this package is done: files/directories to deploy, databases dump to load, cron entry to add,
-SSH public keys of admin to deploy and any other command lines to launch.
+- Can create profiles of packages to define what to deploy when a subscription to this package is done: files or directories to deploy, databases dump to load, cron entry to add,
+SSH public keys of administrator to deploy and any other command lines to launch.
 - Create services that define the plan (which package and option) and price policy to use for invoicing: per application, per user, per Gigabytes or any combination of this.
-- Support free trial period with no credit card required on some plans.
-- Can define the SQL or CLI command for each service to define the quantity to bill (For example a sql request to count the number of customers).
-- Provides URLs for the online subscription of a service.
-- Can decide if customer has MySQL/MariaDB and/or restricted (or not) SSH access to its instance.
-- Each customer has its own system and data environment (jail)
-- Add a system layer to replace the php mail function to track and stop evil users using their instance to try to make Spams.  
+- Option to support free trial periods (with no credit card required) on some plans.
+- Can define the SQL or CLI command for each sold services to define how to count the quantity to bill (For example a SQL request to count the number of users).
+- Provides ready to use URLs for an online subscription to a hosting service by your prospects or customers.
 - Autofill and autodetect country in the subscription page using Geoip.
 - Include a probability of VPN usage for each subscriber (to fight against spammer).
+- Can decide if customer has direct access to the MySQL/MariaDB databse and restricted (or not) SSH access to its instance.
+- Each customer has its own system and data environment (jail)
+- Add a system layer to replace the php mail function to track and stop evil users using their created instance to try to make Spams.  
 - Manage a network of reseller with commission dedicated to each reseller (a reseller has its own URL to register/create a new instances of an application and any customer that use it to create its instance is linked to the reseller. Reseller will gain a commission for each invoice paid by the customer). 
-- Provide a customer dashboard for customers to manage their subscription, download their invoice.
+- Provide a customer dashboard for customers to manage their subscription, download their invoices.
 - Each customer can deploy more applications/services with their existing account.
-- All customer, subscriptions (contracts), invoices are Dolibarr common documents shared with your existing workflow.
-- Payment of customer can be done automatically by credit card using Stripe or by SEPA mandate.
+- All customers, subscriptions (contracts), invoices are Dolibarr common documents shared with your existing data.
+- Payment of customers can be done automatically by credit card using Stripe (not visible by user) or semi-automatic by SEPA mandate.
 - Billing rules (date, amount, frequency of next payment) can be modified differently for each customer.
-- Provide a lot of predefined email templates in server languages for the subscription management (subscription, trial expiration,
-cancellation, ...)
+- Provide a lot of predefined email templates in server languages for the subscription management (subscription, trial expiration, cancellation, ...)
 - Can manage each customer/subscription from Dolibarr backoffice (for example deploy, suspend, unsuspend, undeploy an instance).
 - Provide statistics reports on trial instances, customers, etc.
 
--- htdocs/sellyoursaas/scripts/old_migrate_v1v2.sh
+++ htdocs/sellyoursaas/scripts/old_migrate_v1v2.sh
@@ -3,40 +3,60 @@
 export now=`date +%Y%m%d%H%M%S`
 
 echo
-echo "####################################### ${0} ${1}"
-echo "${0} ${@}"
-echo "# user id --------> $(id -u)"
-echo "# now ------------> $now"
-echo "# PID ------------> ${$}"
-echo "# PWD ------------> $PWD" 
-echo "# arguments ------> ${@}"
-echo "# path to me -----> ${0}"
-echo "# parent path ----> ${0%/*}"
-echo "# my name --------> ${0##*/}"
-echo "# realname -------> $(realpath ${0})"
-echo "# realname name --> $(basename $(realpath ${0}))"
-echo "# realname dir ---> $(dirname $(realpath ${0}))"
+#echo "####################################### ${0} ${1}"
+#echo "${0} ${@}"
+#echo "# user id --------> $(id -u)"
+#echo "# now ------------> $now"
+#echo "# PID ------------> ${$}"
+#echo "# PWD ------------> $PWD" 
+#echo "# arguments ------> ${@}"
+#echo "# path to me -----> ${0}"
+#echo "# parent path ----> ${0%/*}"
+#echo "# my name --------> ${0##*/}"
+#echo "# realname -------> $(realpath ${0})"
+#echo "# realname name --> $(basename $(realpath ${0}))"
+#echo "# realname dir ---> $(dirname $(realpath ${0}))"
 
 export PID=${$}
 export scriptdir=$(dirname $(realpath ${0}))
 export ZONE="on.dolicloud.com.hosts" 
 export REMOTEIP='79.137.96.15'
 
+
+echo "***** $0 *****"
+
 if [ "$(id -u)" != "0" ]; then
 	echo "This script must be run as root" 1>&2
 	exit 1
 fi
 
+> $scriptdir/filetomigrate.ok
+> $scriptdir/filetomigrate.ko
+
 if [ "x$1" == "x" ]; then
 	echo "You must select instances first with this and run then with option confirm:"
 	sql="SELECT name as '#name' from app_instance where status = 'DEPLOYED' AND access_enabled = true AND customer_id IN (select id from customer where manual_collection = false) ORDER BY deployed_date LIMIT 2" 
-	echo "echo \"$sql\" | mysql -Dsaasplex -udebian-sys-maint -pxxx > $scriptdir/filetomigrate.txt"
-	echo "where xxx can be found with:  sudo cat /etc/mysql/debian.cnf |grep password | sort -u | awk ' { print \$3; }'"
+	echo "echo \"$sql\" | mysql -Dsaasplex -uxxx -pyyy -hzzz > $scriptdir/filetomigrate.txt"
+	#echo "where xxx can be found with:  sudo cat /etc/mysql/debian.cnf |grep password | sort -u | awk ' { print \$3; }'"
+	echo
+	echo There is currently `cat $scriptdir/filetomigrate.txt | grep -v '#name' | wc -l` records in filetomigrate.txt
+	echo
+	echo "Usage: $0 confirm"
 	exit 1
 fi
 
 if [[ ! -f $scriptdir/filetomigrate.txt ]]; then
 	echo Error failed to find file $scriptdir/filetomigrate.txt with list of instances to migrate.
+	echo
+	echo "You must select instances first with this and run then with option confirm:"
+	sql="SELECT name as '#name' from app_instance where status = 'DEPLOYED' AND access_enabled = true AND customer_id IN (select id from customer where manual_collection = false) ORDER BY deployed_date LIMIT 2" 
+	echo "echo \"$sql\" | mysql -Dsaasplex -uxxx -pyyy -hzzz > $scriptdir/filetomigrate.txt"
+	#echo "where xxx can be found with:  sudo cat /etc/mysql/debian.cnf |grep password | sort -u | awk ' { print \$3; }'"
+	echo
+	echo There is currently `cat $scriptdir/filetomigrate.txt | grep -v '#name' | wc -l` records in filetomigrate.txt
+	echo
+	echo "Usage: $0 confirm"
+	exit 1
 fi
 
 # Make migration
@@ -45,8 +65,13 @@ for instancename in `cat $scriptdir/filetomigrate.txt | grep -v '#'`
 do
 	if [[ "x$instancename" != "x" ]]; then
 		echo Try to migrate $instance with php old_migrate_v1v2.php $instancename $instancename confirm
-		#php old_migrate_v1v2.php $instancename $instancename confirm
+		php old_migrate_v1v2.php $instancename $instancename confirm
 		result=$?
+		if [[ "x$result" == "x0" ]]; then
+			echo $instancename > $scriptdir/filetomigrate.ok
+		else
+			echo $instancename > $scriptdir/filetomigrate.ko
+		fi
 		echo Result = $result
 	fi
 done
@@ -54,7 +79,7 @@ done
 
 # Fix DNS
 echo "----- Change DNS"
-for instancename in `cat $scriptdir/filetomigrate.txt | grep -v '#'`
+for instancename in `cat $scriptdir/filetomigrate.ok | grep -v '#'`
 do
 	echo `date +%Y%m%d%H%M%S`" **** Archive file with cp /etc/bind/${ZONE} /etc/bind/archives/${ZONE}-$now"
 	cp /etc/bind/${ZONE} /etc/bind/archives/${ZONE}-$now
@@ -78,15 +103,15 @@ done
 
 # Disable V1
 echo "----- Disable V1 by switching to manual collection"
-for instancename in `cat $scriptdir/filetomigrate.txt | grep -v '#'`
+sql="UPDATE customer set manual_collection = true where id IN (SELECT customer_id FROM app_instance WHERE name IN ("
+for instancename in `cat $scriptdir/filetomigrate.ok | grep -v '#'`
 do
-	sql="UPDATE customer set manual_collection = true where id IN (SELECT customer_id FROM app_instance WHERE name IN ("
 	if [[ "x$instancename" != "x" ]]; then
 		sql="$sql'$instancename.on.dolicloud.com'," 
 	fi
-	sql="$sql'bidon'));"
-	echo "echo \"$sql\" | mysql -Dsaasplex -udebian-sys-maint -pxxx"
-	echo "where xxx can be found with:  sudo cat /etc/mysql/debian.cnf |grep password | sort -u | awk ' { print \$3; }'"
 done
+sql="$sql'bidon'));"
+echo "WARNING !!! Now you must flag all successfuly migrated instances as migrated on source with:"
+echo "echo \"$sql\" | mysql -Dsaasplex -uxxx -pyyy -hzzz"
 
 
