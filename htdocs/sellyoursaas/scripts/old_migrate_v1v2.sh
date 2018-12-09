53865f09b4f384fe218339b77c4988c5fb3c3827
Laurent Destailleur <eldy@destailleur.fr>
2018-12-09 21:13:54 +0100

    Exclude comment

--- htdocs/sellyoursaas/scripts/old_migrate_v1v2.sh
+++ htdocs/sellyoursaas/scripts/old_migrate_v1v2.sh
@@ -53,7 +53,7 @@ if [[ ! -f $scriptdir/filetomigrate.txt ]]; then
 	echo "echo \"$sql\" | mysql -Dsaasplex -uxxx -pyyy -hzzz > $scriptdir/filetomigrate.txt"
 	#echo "where xxx can be found with:  sudo cat /etc/mysql/debian.cnf |grep password | sort -u | awk ' { print \$3; }'"
 	echo
-	echo There is currently `cat $scriptdir/filetomigrate.txt | grep -v '#name' | wc -l` records in filetomigrate.txt
+	echo There is currently `cat $scriptdir/filetomigrate.txt | grep -v '#' | wc -l` records in filetomigrate.txt
 	echo
 	echo "Usage: $0 confirm"
 	exit 1
