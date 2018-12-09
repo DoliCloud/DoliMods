710dd7023791718486c51b6a279b076d5b72d479
Laurent Destailleur <eldy@destailleur.fr>
2018-12-09 21:36:32 +0100

    Fix more options

--- htdocs/sellyoursaas/scripts/old_migrate_v1v2.sh
+++ htdocs/sellyoursaas/scripts/old_migrate_v1v2.sh
@@ -41,7 +41,7 @@ if [ "x$1" == "x" ]; then
 	echo
 	echo There is currently `cat $scriptdir/filetomigrate.txt | grep -v '#' | wc -l` records in filetomigrate.txt
 	echo
-	echo "Usage: $0 confirm"
+	echo "Usage: $0 confirm|migrate|dns|disablev1"
 	exit 1
 fi
 
@@ -60,59 +60,63 @@ if [[ ! -f $scriptdir/filetomigrate.txt ]]; then
 fi
 
 # Make migration
-echo "----- Make migration. Loop on $scriptdir/filetomigrate.txt"
-for instancename in `cat $scriptdir/filetomigrate.txt | sed -e 's!.on.dolicloud.com!!g' | grep -v '#'`
-do
-	if [[ "x$instancename" != "x" ]]; then
-		echo Try to migrate $instance with php old_migrate_v1v2.php $instancename $instancename confirm
-		php old_migrate_v1v2.php $instancename $instancename confirm
-		result=$?
-		if [[ "x$result" == "x0" ]]; then
-			echo $instancename >> $scriptdir/filetomigrate.ok
-		else
-			echo $instancename >> $scriptdir/filetomigrate.ko
+if [ "x$1" == "xconfirm" -o "x$1" == "xmigrate" ]; then
+	echo "----- Make migration. Loop on $scriptdir/filetomigrate.txt"
+	for instancename in `cat $scriptdir/filetomigrate.txt | sed -e 's!.on.dolicloud.com!!g' | grep -v '#'`
+	do
+		if [[ "x$instancename" != "x" ]]; then
+			echo Try to migrate $instance with php old_migrate_v1v2.php $instancename $instancename confirm
+			php old_migrate_v1v2.php $instancename $instancename confirm
+			result=$?
+			if [[ "x$result" == "x0" ]]; then
+				echo $instancename >> $scriptdir/filetomigrate.ok
+			else
+				echo $instancename >> $scriptdir/filetomigrate.ko
+			fi
+			echo Result = $result
 		fi
-		echo Result = $result
-	fi
-done
-
+	done
+fi
 
 # Fix DNS
-echo "----- Change DNS of file into $scriptdir/filetomigrate.ok"
-for instancename in `cat $scriptdir/filetomigrate.ok | sed -e 's!.on.dolicloud.com!!g' | grep -v '#'`
-do
-	echo `date +%Y%m%d%H%M%S`" **** Archive file with cp /etc/bind/${ZONE} /etc/bind/archives/${ZONE}-$now"
-	cp /etc/bind/${ZONE} /etc/bind/archives/${ZONE}-$now
-
-	if [[ "x$instancename" != "x" ]]; then
-		
-		echo Remove and add DNS for $instancename
-		
-		echo "cat /etc/bind/${ZONE} | grep -v '^$instancename ' > /tmp/${ZONE}.$PID"
-		cat /etc/bind/${ZONE} | grep -v "^$instancename " > /tmp/${ZONE}.$PID
-
-		echo `date +%Y%m%d%H%M%S`" ***** Add $instancename A $REMOTEIP into tmp host file /tmp/${ZONE}.$PID"
-		echo $instancename A $REMOTEIP >> /tmp/${ZONE}.$PID  
-
-		echo `date +%Y%m%d%H%M%S`" **** Move new host file with mv -fu /tmp/${ZONE}.$PID /etc/bind/${ZONE}"
-		mv -fu /tmp/${ZONE}.$PID /etc/bind/${ZONE}
-
-	fi
-done
-
+if [ "x$1" == "xconfirm" -o "x$1" == "xdns" ]; then
+	echo "----- Change DNS of file into $scriptdir/filetomigrate.ok"
+	for instancename in `cat $scriptdir/filetomigrate.ok | sed -e 's!.on.dolicloud.com!!g' | grep -v '#'`
+	do
+		echo `date +%Y%m%d%H%M%S`" **** Archive file with cp /etc/bind/${ZONE} /etc/bind/archives/${ZONE}-$now"
+		cp /etc/bind/${ZONE} /etc/bind/archives/${ZONE}-$now
+	
+		if [[ "x$instancename" != "x" ]]; then
+			
+			echo Remove and add DNS for $instancename
+			
+			echo "cat /etc/bind/${ZONE} | grep -v '^$instancename ' > /tmp/${ZONE}.$PID"
+			cat /etc/bind/${ZONE} | grep -v "^$instancename " > /tmp/${ZONE}.$PID
+	
+			echo `date +%Y%m%d%H%M%S`" ***** Add $instancename A $REMOTEIP into tmp host file /tmp/${ZONE}.$PID"
+			echo $instancename A $REMOTEIP >> /tmp/${ZONE}.$PID  
+	
+			echo `date +%Y%m%d%H%M%S`" **** Move new host file with mv -fu /tmp/${ZONE}.$PID /etc/bind/${ZONE}"
+			mv -fu /tmp/${ZONE}.$PID /etc/bind/${ZONE}
+	
+		fi
+	done
+fi
 
 # Disable V1
-echo "----- Disable V1 by switching to manual collection"
-sql="UPDATE customer set manual_collection = true where id IN (SELECT customer_id FROM app_instance WHERE name IN ("
-for instancename in `cat $scriptdir/filetomigrate.ok | sed -e 's!.on.dolicloud.com!!g' | grep -v '#'`
-do
-	if [[ "x$instancename" != "x" ]]; then
-		sql="$sql'$instancename.on.dolicloud.com'," 
-	fi
-done
-sql="$sql'bidon'));"
-echo "WARNING !!! Now you must flag all successfuly migrated instances as migrated on source with:"
-echo "echo \"$sql\" | mysql -Dsaasplex -uxxx -pyyy -hzzz"
-echo "WARNING !!! You must increase serial of DNS file /etc/bind/on.dolicloud.com.hosts and restart with:"
-echo "rndc reload on.dolicloud.com"
+if [ "x$1" == "xconfirm" -o "x$1" == "xdisablev1" ]; then
+	echo "----- Disable V1 by switching to manual collection"
+	sql="UPDATE customer set manual_collection = true where id IN (SELECT customer_id FROM app_instance WHERE name IN ("
+	for instancename in `cat $scriptdir/filetomigrate.ok | sed -e 's!.on.dolicloud.com!!g' | grep -v '#'`
+	do
+		if [[ "x$instancename" != "x" ]]; then
+			sql="$sql'$instancename.on.dolicloud.com'," 
+		fi
+	done
+	sql="$sql'bidon'));"
+	echo "WARNING !!! Now you must flag all successfuly migrated instances as migrated on source with:"
+	echo "echo \"$sql\" | mysql -Dsaasplex -uxxx -pyyy -hzzz"
+	echo "WARNING !!! You must increase serial of DNS file /etc/bind/on.dolicloud.com.hosts and restart with:"
+	echo "rndc reload on.dolicloud.com"
+fi
 
