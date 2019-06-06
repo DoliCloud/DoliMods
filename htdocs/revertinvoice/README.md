250a74db7634f01a70c9c78c14af88cfef1ef3e2
Laurent Destailleur <eldy@users.sourceforge.net>
2019-06-06 19:58:23 +0200

    Work on revertinvoice

---
+++ htdocs/revertinvoice/README.md
@@ -0,0 +1,92 @@
+# REVERTINVOICE FOR <a href="https://www.dolibarr.org">DOLIBARR ERP CRM</a>
+
+## Features
+Create a supplier invoice on another entity, each time a customer invoice is created on a thirdparty
+that is configured to be reverted.
+
+<!--
+![Screenshot revertinvoice](img/screenshot_revertinvoice.png?raw=true "RevertInvoice"){imgmd}
+-->
+
+Other modules are available on <a href="https://www.dolistore.com" target="_new">Dolistore.com</a>.
+
+
+
+## Translations
+
+Translations can be define manually by editing files into directories *langs*. 
+
+<!--
+This module contains also a sample configuration for Transifex, under the hidden directory [.tx](.tx), so it is possible to manage translation using this service. 
+
+For more informations, see the [translator's documentation](https://wiki.dolibarr.org/index.php/Translator_documentation).
+
+There is a [Transifex project](https://transifex.com/projects/p/dolibarr-module-template) for this module.
+-->
+
+
+<!--
+
+## Installation
+
+### From the ZIP file and GUI interface
+
+- If you get the module in a zip file (like when downloading it from the market place [Dolistore](https://www.dolistore.com)), go into
+menu ```Home - Setup - Modules - Deploy external module``` and upload the zip file.
+
+
+Note: If this screen tell you there is no custom directory, check your setup is correct: 
+
+- In your Dolibarr installation directory, edit the ```htdocs/conf/conf.php``` file and check that following lines are not commented:
+
+    ```php
+    //$dolibarr_main_url_root_alt ...
+    //$dolibarr_main_document_root_alt ...
+    ```
+
+- Uncomment them if necessary (delete the leading ```//```) and assign a sensible value according to your Dolibarr installation
+
+    For example :
+
+    - UNIX:
+        ```php
+        $dolibarr_main_url_root_alt = '/custom';
+        $dolibarr_main_document_root_alt = '/var/www/Dolibarr/htdocs/custom';
+        ```
+
+    - Windows:
+        ```php
+        $dolibarr_main_url_root_alt = '/custom';
+        $dolibarr_main_document_root_alt = 'C:/My Web Sites/Dolibarr/htdocs/custom';
+        ```
+        
+### From a GIT repository
+
+- Clone the repository in ```$dolibarr_main_document_root_alt/revertinvoice```
+
+```sh
+cd ....../custom
+git clone git@github.com:gitlogin/revertinvoice.git revertinvoice
+```
+
+### <a name="final_steps"></a>Final steps
+
+From your browser:
+
+  - Log into Dolibarr as a super-administrator
+  - Go to "Setup" -> "Modules"
+  - You should now be able to find and enable the module
+
+
+-->
+
+
+## Licenses
+
+**Main code**
+
+GPLv3 or (at your option) any later version. See file COPYING for more information.
+
+**Documentation**
+
+All texts and readmes are licensed under GFDL.
---
+++ htdocs/revertinvoice/admin/setup.php
@@ -0,0 +1,172 @@
+<?php
+/* Copyright (C) 2004-2017 Laurent Destailleur  <eldy@users.sourceforge.net>
+ * Copyright (C) 2019 Alice Adminson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file    revertinvoice/admin/setup.php
+ * \ingroup revertinvoice
+ * \brief   RevertInvoice setup page.
+ */
+
+// Load Dolibarr environment
+$res=0;
+// Try main.inc.php into web root known defined into CONTEXT_DOCUMENT_ROOT (not always defined)
+if (! $res && ! empty($_SERVER["CONTEXT_DOCUMENT_ROOT"])) $res=@include $_SERVER["CONTEXT_DOCUMENT_ROOT"]."/main.inc.php";
+// Try main.inc.php into web root detected using web root calculated from SCRIPT_FILENAME
+$tmp=empty($_SERVER['SCRIPT_FILENAME'])?'':$_SERVER['SCRIPT_FILENAME'];$tmp2=realpath(__FILE__); $i=strlen($tmp)-1; $j=strlen($tmp2)-1;
+while($i > 0 && $j > 0 && isset($tmp[$i]) && isset($tmp2[$j]) && $tmp[$i]==$tmp2[$j]) { $i--; $j--; }
+if (! $res && $i > 0 && file_exists(substr($tmp, 0, ($i+1))."/main.inc.php")) $res=@include substr($tmp, 0, ($i+1))."/main.inc.php";
+if (! $res && $i > 0 && file_exists(dirname(substr($tmp, 0, ($i+1)))."/main.inc.php")) $res=@include dirname(substr($tmp, 0, ($i+1)))."/main.inc.php";
+// Try main.inc.php using relative path
+if (! $res && file_exists("../../main.inc.php")) $res=@include "../../main.inc.php";
+if (! $res && file_exists("../../../main.inc.php")) $res=@include "../../../main.inc.php";
+if (! $res) die("Include of main fails");
+
+global $langs, $user;
+
+// Libraries
+require_once DOL_DOCUMENT_ROOT . "/core/lib/admin.lib.php";
+require_once '../lib/revertinvoice.lib.php';
+//require_once "../class/myclass.class.php";
+
+// Translations
+$langs->loadLangs(array("admin", "revertinvoice@revertinvoice"));
+
+// Access control
+if (! $user->admin) accessforbidden();
+
+// Parameters
+$action = GETPOST('action', 'alpha');
+$backtopage = GETPOST('backtopage', 'alpha');
+
+$revertinvoicethirdpartyid = GETPOST('revertinvoicethirdpartyid', 'int');
+$revertinvoiceentityid = GETPOST('revertinvoiceentityid', 'int');
+
+$arrayofparameters=array(
+	'REVERTINVOICE_MYPARAM1'=>array('css'=>'minwidth200','enabled'=>1),
+	'REVERTINVOICE_MYPARAM2'=>array('css'=>'minwidth500','enabled'=>1)
+);
+
+
+
+/*
+ * Actions
+ */
+
+if ((float) DOL_VERSION >= 6)
+{
+	include DOL_DOCUMENT_ROOT.'/core/actions_setmoduleoptions.inc.php';
+}
+
+if ($action == 'addrevertinvoice')
+{
+    if ($revertinvoicethirdpartyid > 0)
+    {
+        $result = dolibarr_set_const($db, 'REVERTINVOICE_THIRDPARTYID_'.$revertinvoicethirdpartyid, $revertinvoiceentityid, 'chaine', 0, '', 0);
+    }
+    else
+    {
+        setEventMessages('Select a thirdparty', null, 'errors');
+    }
+}
+
+if ($action == 'delete')
+{
+    if ($revertinvoicethirdpartyid > 0)
+    {
+        $const = 'REVERTINVOICE_THIRDPARTYID_'.$revertinvoicethirdpartyid;
+        var_dump($const);
+        $result = dolibarr_del_const($db, $const, 0);
+
+        header("Location: ".$_SERVER["PHP_SELF"]);
+        exit;
+    }
+}
+
+
+/*
+ * View
+ */
+
+$form = new Form($db);
+
+$page_name = "RevertInvoiceSetup";
+llxHeader('', $langs->trans($page_name));
+
+// Subheader
+$linkback = '<a href="'.($backtopage?$backtopage:DOL_URL_ROOT.'/admin/modules.php?restore_lastsearch_values=1').'">'.$langs->trans("BackToModuleList").'</a>';
+
+print load_fiche_titre($langs->trans($page_name), $linkback, 'object_revertinvoice@revertinvoice');
+
+// Configuration header
+$head = revertinvoiceAdminPrepareHead();
+dol_fiche_head($head, 'settings', '', -1, "revertinvoice@revertinvoice");
+
+// Setup page goes here
+echo '<span class="opacitymedium">'.$langs->trans("RevertInvoiceSetupPage").'</span><br><br>';
+
+
+print '<form method="POST" action="'.$_SERVER["PHP_SELF"].'">';
+print '<input type="hidden" name="token" value="'.$_SESSION['newtoken'].'">';
+print '<input type="hidden" name="action" value="addrevertinvoice">';
+
+print '<table class="noborder" width="100%">';
+print '<tr class="liste_titre"><td class="titlefield">'.$langs->trans("ThirdParty").'</td><td>'.$langs->trans("Entity").'</td><td></td></tr>';
+
+print '<tr class="oddeven"><td>';
+print $form->select_company('', 'revertinvoicethirdpartyid');
+print '</td><td>';
+print '<input type="text" name="revertinvoiceentityid" value="1">';
+print '</td><td>';
+print '<input class="button" type="submit" value="'.$langs->trans("Add").'">';
+print '</td></tr>';
+
+$i=0;
+
+foreach($conf->global as $key => $value)
+{
+    if (preg_match('/REVERTINVOICE_THIRDPARTYID_(.*)/', $key, $reg))
+    {
+        $thirdpartyid = $reg[1];
+        $const = 'REVERTINVOICE_THIRPDARTYID_'.$thirdpartyid;
+        $entityid = $value;
+
+        $tmpcompany = new Societe($db);
+        $tmpcompany->fetch($thirdpartyid);
+
+        print '<tr class="oddeven"><td>';
+        print $tmpcompany->getNomUrl(1);
+        print '</td><td>';
+        print $value;
+        print '</td><td>';
+        print '<a href="'.$_SERVER['PHP_SELF'].'?action=delete&revertinvoicethirdpartyid='.$thirdpartyid.'">';
+        print img_delete();
+        print '</a>';
+        print '</td></tr>';
+    }
+}
+print '</table>';
+
+print '</form>';
+print '<br>';
+
+
+// Page end
+dol_fiche_end();
+
+llxFooter();
+$db->close();
