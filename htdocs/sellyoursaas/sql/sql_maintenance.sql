d61db2f17c5fc87e98fe3afbb9eec62111032393
Laurent Destailleur <eldy@destailleur.fr>
2019-07-04 03:02:02 +0200

    Add doc of project

--- htdocs/sellyoursaas/sql/sql_maintenance.sql
+++ htdocs/sellyoursaas/sql/sql_maintenance.sql
@@ -15,6 +15,10 @@ select * from dolibarr.llx_contrat_extrafields, information_schema.processlist w
 --select concat('KILL ',information_schema.processlist.id,';') from dolibarr.llx_contrat_extrafields, information_sch:qema.processlist where dolibarr.llx_contrat_extrafields.username_db = information_schema.processlist.user and TIME > 1800 into outfile '/tmp/mkill.txt';
 --source /tmp/a.txt;
 
+-- List nb of running connections
+mysqladmin -uaaa -paaa -i1  extended | grep Threads_running
+
+
 
 -- Instance with incorrect entry localhost
 select * from dolibarr.llx_contrat_extrafields where username_db in (select user from mysql.user where Host = 'localhost' and password = '' and user like 'dbu%');
