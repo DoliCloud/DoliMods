bf93c40a24e6d980116a4469dc59522db10060b3
Laurent Destailleur <eldy@destailleur.fr>
2019-10-06 17:01:54 +0200

    Fix link to toggle tawkto

--- htdocs/tawkto/class/actions_tawkto.class.php
+++ htdocs/tawkto/class/actions_tawkto.class.php
@@ -52,8 +52,9 @@ class ActionsTawkto
 		}
 
 		$out.= '<div class="inline-block"><div class="classfortooltip inline-block login_block_elem inline-block" style="padding: 0px; padding: 0px; padding-right: 3px !important;" title="'.dol_escape_htmltag($tooltiptext).'">';
-
-		$out.= '<a href="'.$_SERVER['PHP_SELF'].'?tawktotoggle=1"><span class="fa '.$fontas.' atoplogin"></span></a>';
+		$param = '';
+		if (! empty($_SERVER["QUERY_STRING"])) $param = preg_replace('/&?tawktotoggle=\d/', '', $_SERVER["QUERY_STRING"]);
+		$out.= '<a href="'.$_SERVER['PHP_SELF'].'?'.($param?$param.'&':'').'tawktotoggle=1"><span class="fa '.$fontas.' atoplogin"></span></a>';
 		$out.= '</div></div>';	// Do not ouptu "\n" here, it create a space into toolbar
 
 		$this->resprints = $out;
