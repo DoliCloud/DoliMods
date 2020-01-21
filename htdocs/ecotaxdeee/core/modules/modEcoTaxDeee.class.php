426299d0b455b7c24d3b3af90eb39f56eaf9f3ae
Laurent Destailleur <eldy@destailleur.fr>
2020-01-21 19:01:55 +0100

    Add dependency on modProduct

--- htdocs/ecotaxdeee/core/modules/modEcoTaxDeee.class.php
+++ htdocs/ecotaxdeee/core/modules/modEcoTaxDeee.class.php
@@ -83,8 +83,8 @@ class modEcoTaxDeee extends DolibarrModules
 		$this->config_page_url = array('index.php@ecotaxdeee');
 
 		// Dependencies
-		$this->depends = array();		// List of modules id that must be enabled if this module is enabled
-		$this->requiredby = array();	// List of modules id to disable if this one is disabled
+		$this->depends = array('modProduct');		// List of modules id that must be enabled if this module is enabled
+		$this->requiredby = array();	             // List of modules id to disable if this one is disabled
 		$this->phpmin = array(4,1);					// Minimum version of PHP required by module
 		$this->need_dolibarr_version = array(3,4,2);	// Minimum version of Dolibarr required by module
 		$this->langfiles = array("ecotaxdeee@ecotaxdeee");
