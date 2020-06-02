94ea706fd231388f01d3ec43760c2172b72fbd7b
Laurent Destailleur <eldy@destailleur.fr>
2020-06-02 15:13:20 +0200

    Removed not used code

--- htdocs/captureserver/captureserver_card.php
+++ htdocs/captureserver/captureserver_card.php
@@ -489,16 +489,6 @@ if ($object->id > 0 && (empty($action) || ($action != 'edit' && $action != 'crea
 	    print '<div class="fichecenter"><div class="fichehalfleft">';
 	    print '<a name="builddoc"></a>'; // ancre
 
-	    // Documents
-	    /*$objref = dol_sanitizeFileName($object->ref);
-	    $relativepath = $comref . '/' . $comref . '.pdf';
-	    $filedir = $conf->captureserver->dir_output . '/' . $objref;
-	    $urlsource = $_SERVER["PHP_SELF"] . "?id=" . $object->id;
-	    $genallowed = $user->rights->captureserver->read;	// If you can read, you can build the PDF to read content
-	    $delallowed = $user->rights->captureserver->create;	// If you can create/edit, you can remove a file on card
-	    print $formfile->showdocuments('captureserver', $objref, $filedir, $urlsource, $genallowed, $delallowed, $object->modelpdf, 1, 0, 0, 28, 0, '', '', '', $soc->default_lang);
-		*/
-
 	    // Show links to link elements
 	    //$linktoelem = $form->showLinkToObjectBlock($object, null, array('captureserver'));
 	    //$somethingshown = $form->showLinkedObjectBlock($object, $linktoelem);
