bebf89d655d755bd9e6e505cff49c4967cd9e76c
Laurent Destailleur <eldy@destailleur.fr>
2021-06-10 00:16:47 +0200

    Use getURLContent (removed duplicated method)

--- htdocs/google/lib/google_contact.lib.php
+++ htdocs/google/lib/google_contact.lib.php
@@ -623,7 +623,7 @@ function googleUpdateContact($client, $contactId, &$object, $useremail = 'defaul
 		$addheaderscurl=array('If-Match: *', 'GData-Version: 3.0', 'Authorization: Bearer '.$access_token, 'Content-Type: application/atom+xml');
 
 		// update entry
-		$response = getURLContentBis('https://www.google.com/m8/feeds/contacts/'.urlencode($useremail).'/base/'.$newcontactid, 'PUT', $xmlStr, 1, $addheaderscurl);   // We must use getURLContentBis with PUT or getURLContent with PUTALREADYFORMATED with 3.7.2+
+		$response = getURLContent('https://www.google.com/m8/feeds/contacts/'.urlencode($useremail).'/base/'.$newcontactid, 'PUTALREADYFORMATED', $xmlStr, 1, $addheaderscurl);
 		if ($response['content']) {
 			try {
 				$document = new DOMDocument("1.0", "utf-8");
@@ -703,7 +703,7 @@ function googleDeleteContactByRef($client, $ref, $useremail = 'default')
 		$addheaderscurl=array('GData-Version: 3.0', 'If-Match: *', 'Authorization: Bearer '.$access_token);
 		//$useremail='default';
 
-		$result = getURLContentBis('https://www.google.com/m8/feeds/contacts/'.urlencode($useremail).'/full/'.$newcontactid, 'DELETE', '', 0, $addheaderscurl);
+		$result = getURLContent('https://www.google.com/m8/feeds/contacts/'.urlencode($useremail).'/full/'.$newcontactid, 'DELETE', '', 0, $addheaderscurl);
 		$xmlStr=$result['content'];
 
 		return '';
@@ -1141,113 +1141,6 @@ function insertGContactGroup($gdata, $groupName, $useremail = 'default')
 	}
 
 	return($id);
-}
-
-
-
-
-
-/**
- * Function get content from an URL (use proxy if proxy defined).
- * This is a duplicate of function but adding the DELETE because DELETE was missing in 3.7.*
- *
- * @param	string	$url 				URL to call.
- * @param	string	$postorget			'POST', 'GET', 'HEAD'
- * @param	string	$param				Parameters of URL (x=value1&y=value2)
- * @param	string	$followlocation		1=Follow location, 0=Do not follow
- * @param	array	$addheaders			Array of string to add into header. Example: array('Accept: application/xrds+xml', ....)
- * @return	array						Returns an associative array containing the response from the server array('content'=>response,'curl_error_no'=>errno,'curl_error_msg'=>errmsg...)
- */
-function getURLContentBis($url, $postorget = 'GET', $param = '', $followlocation = 1, $addheaders = array())
-{
-	//declaring of global variables
-	global $conf, $langs;
-	$USE_PROXY=empty($conf->global->MAIN_PROXY_USE)?0:$conf->global->MAIN_PROXY_USE;
-	$PROXY_HOST=empty($conf->global->MAIN_PROXY_HOST)?0:$conf->global->MAIN_PROXY_HOST;
-	$PROXY_PORT=empty($conf->global->MAIN_PROXY_PORT)?0:$conf->global->MAIN_PROXY_PORT;
-	$PROXY_USER=empty($conf->global->MAIN_PROXY_USER)?0:$conf->global->MAIN_PROXY_USER;
-	$PROXY_PASS=empty($conf->global->MAIN_PROXY_PASS)?0:$conf->global->MAIN_PROXY_PASS;
-
-	dol_syslog("getURLContentBis postorget=".$postorget." URL=".$url." param=".$param);
-
-	//setting the curl parameters.
-	$ch = curl_init();
-
-	/*print $API_Endpoint."-".$API_version."-".$PAYPAL_API_USER."-".$PAYPAL_API_PASSWORD."-".$PAYPAL_API_SIGNATURE."<br>";
-	 print $USE_PROXY."-".$gv_ApiErrorURL."<br>";
-	 print $nvpStr;
-	 exit;*/
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_VERBOSE, 1);
-	curl_setopt($ch, CURLOPT_USERAGENT, 'Dolibarr geturl function');
-
-	@curl_setopt($ch, CURLOPT_FOLLOWLOCATION, ($followlocation?true:false));   // We use @ here because this may return warning if safe mode is on or open_basedir is on
-
-	if (count($addheaders)) curl_setopt($ch, CURLOPT_HTTPHEADER, $addheaders);
-	curl_setopt($ch, CURLINFO_HEADER_OUT, true);	// To be able to retrieve request header and log it
-
-	// $conf->global->GOOGLE_SSLVERSION should be set to 1 to use TLSv1 by default or change to TLSv1.2 in module configuration
-	if (isset($conf->global->GOOGLE_SSLVERSION)) curl_setopt($ch, CURLOPT_SSLVERSION, $conf->global->GOOGLE_SSLVERSION);
-
-	//turning off the server and peer verification(TrustManager Concept).
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, empty($conf->global->MAIN_USE_CONNECT_TIMEOUT)?5:$conf->global->MAIN_USE_CONNECT_TIMEOUT);
-	curl_setopt($ch, CURLOPT_TIMEOUT, empty($conf->global->MAIN_USE_RESPONSE_TIMEOUT)?30:$conf->global->MAIN_USE_RESPONSE_TIMEOUT);
-
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);		// We want response
-	if ($postorget == 'POST') {
-		curl_setopt($ch, CURLOPT_POST, 1);	// POST
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $param);	// Setting param x=a&y=z as POST fields
-	} elseif ($postorget == 'PUT') {
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT'); // HTTP request is 'PUT'
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $param);	// Setting param x=a&y=z as PUT fields
-	} elseif ($postorget == 'HEAD') {
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'HEAD'); // HTTP request is 'HEAD'
-		curl_setopt($ch, CURLOPT_NOBODY, true);
-	} elseif ($postorget == 'DELETE') {
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');	// POST
-	} else {
-		curl_setopt($ch, CURLOPT_POST, 0);			// GET
-	}
-
-	//if USE_PROXY constant set to TRUE in Constants.php, then only proxy will be enabled.
-	if ($USE_PROXY) {
-		dol_syslog("getURLContentBis set proxy to ".$PROXY_HOST. ":" . $PROXY_PORT." - ".$PROXY_USER. ":" . $PROXY_PASS);
-		//curl_setopt ($ch, CURLOPT_PROXYTYPE, CURLPROXY_HTTP); // Curl 7.10
-		curl_setopt($ch, CURLOPT_PROXY, $PROXY_HOST. ":" . $PROXY_PORT);
-		if ($PROXY_USER) curl_setopt($ch, CURLOPT_PROXYUSERPWD, $PROXY_USER. ":" . $PROXY_PASS);
-	}
-
-	//getting response from server
-	$response = curl_exec($ch);
-
-	$status = curl_getinfo($ch, CURLINFO_HEADER_OUT);	// Reading of request must be done after sending request
-	dol_syslog("getURLContentBis request=".$status);
-
-	dol_syslog("getURLContentBis response=".$response);
-
-	$rep=array();
-	$rep['content']=$response;
-	$rep['curl_error_no']='';
-	$rep['curl_error_msg']='';
-
-	if (curl_errno($ch)) {
-		// moving to display page to display curl errors
-		$rep['curl_error_no']=curl_errno($ch);
-		$rep['curl_error_msg']=curl_error($ch);
-
-		dol_syslog("getURLContentBis curl_error array is ".join(',', $rep));
-	} else {
-		$info = curl_getinfo($ch);
-		$rep['header_size']=$info['header_size'];
-
-		//closing the curl
-		curl_close($ch);
-	}
-
-	return $rep;
 }
 
 
