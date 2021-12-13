9813f3ab7047045e2512598ae2b13e805544004a
Laurent Destailleur <eldy@destailleur.fr>
2021-12-13 08:57:59 +0100

    Removed useless CSS

--- htdocs/ovh/core/modules/modOvh.class.php
+++ htdocs/ovh/core/modules/modOvh.class.php
@@ -79,7 +79,7 @@ class modOvh extends DolibarrModules
 			'tpl' => 0,                                      	// Set this to 1 if module overwrite template dir (core/tpl)
 			'barcode' => 0,                                  	// Set this to 1 if module has its own barcode directory (core/modules/barcode)
 			'models' => 0,                                   	// Set this to 1 if module has its own models directory (core/modules/xxx)
-			'css' => array('/ovh/css/ovh.css.php'),	            // Set this to relative path of css file if module has its own css file
+			'css' => array(),						            // Set this to relative path of css file if module has its own css file
 			'js' => array(),                                    // Set this to relative path of js file if module must load a js on all pages
 			'hooks' => array() 	                                // Set here all hooks context managed by module. You can also set hook context 'all'
 		);
