18e0ae0d1c0403aebc12c3d2993fd2d26502711d
Laurent Destailleur <eldy@destailleur.fr>
2024-06-20 17:14:52 +0200

    Add module agenda

--- htdocs/alumni/core/modules/modAlumni.class.php
+++ htdocs/alumni/core/modules/modAlumni.class.php
@@ -118,7 +118,7 @@ class modAlumni extends DolibarrModules
 		// A condition to hide module
 		$this->hidden = false;
 		// List of module class names that must be enabled if this module is enabled. Example: array('always'=>array('modModuleToEnable1','modModuleToEnable2'), 'FR'=>array('modModuleToEnableFR')...)
-		$this->depends = array('modOpenSurvey', 'modWebsite', 'modMailing');
+		$this->depends = array('modAgenda', 'modEventOrganization', 'modOpenSurvey', 'modWebsite', 'modMailing');
 		// List of module class names to disable if this one is disabled. Example: array('modModuleToDisable1', ...)
 		$this->requiredby = array();
 		// List of module class names this module is in conflict with. Example: array('modModuleToDisable1', ...)
