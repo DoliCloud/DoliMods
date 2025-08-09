3612b2436b67a109e73a426cca3fd8b2e6097dc0
ldestailleur <eldy@destailleur.fr>
2025-08-09 14:01:40 +0200

    Fiw CI

--- htdocs/ovh/class/ovhserver.class.php
+++ htdocs/ovh/class/ovhserver.class.php
@@ -117,7 +117,7 @@ class OvhServer extends CommonObject
 	/**
 	 * Constructor
 	 *
-	 * @param DoliDb $db Database handler
+	 * @param DoliDB $db Database handler
 	 */
 	public function __construct(DoliDB $db)
 	{
