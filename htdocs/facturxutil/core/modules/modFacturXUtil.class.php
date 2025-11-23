10dc7d827b79a2ee841393925c53da8672b0997a
Laurent Destailleur <eldy@destailleur.fr>
2025-11-23 21:15:14 +0100

    Logo

--- htdocs/facturxutil/core/modules/modFacturXUtil.class.php
+++ htdocs/facturxutil/core/modules/modFacturXUtil.class.php
@@ -78,7 +78,7 @@ class modFacturXUtil extends DolibarrModules
 		// Name of image file used for this module.
 		// If file is in theme/yourtheme/img directory under name object_pictovalue.png, use this->picto='pictovalue'
 		// If file is in module/img directory under name object_pictovalue.png, use this->picto='pictovalue@module'
-		$this->picto='pdf';
+		$this->picto = 'fa-file-pdf_far_#65953d';
 
 		// Define some features supported by module (triggers, login, substitutions, menus, css, etc...)
 		$this->module_parts = array(
